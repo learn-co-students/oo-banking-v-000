7351b26277613e89e8556950e73001612853140e
Louiefigz <figueroa.aaron@gmail.com>
2015-11-23 22:32:50 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,9 +11,33 @@ class Transfer
   end 
 
   def both_valid?
-
    sender.valid? && receiver.valid? ? true : false 
-
   end 
+
+
+  def execute_transaction
+    
+    if self.status != "complete" && both_valid? && sender.balance > amount 
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end 
+  end
+
+  def reject_transfer
+    self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+  end 
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += amount 
+      receiver.balance -= amount
+      self.status = "reversed"
+    end 
+  end 
+
 
 end
