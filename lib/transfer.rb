0e8e6feae2e28faf68cfdaa14f17fb30256368b9
Louiefigz <figueroa.aaron@gmail.com>
2015-11-23 20:42:26 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Transfer
-  # code here
+
+  attr_accessor :status, :amount, :receiver, :sender
+    
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver
+    @amount = amount 
+    @status = "pending"
+  end 
+
+  def both_valid?
+
+   @sender.valid? && @receiver.valid? ? true : false 
+
+  
+
+  end 
+
 end
