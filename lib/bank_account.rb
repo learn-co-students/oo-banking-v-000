0050a38a784f5ec26637d8572e3a1c8f665fc29b
authorbeard <msnolan@gmail.com>
2015-12-10 12:33:09 -0500

    still trouble with repeating executed transactions

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,50 @@
+require 'pry'
+
 class BankAccount
-  # code here
+  attr_accessor :status, :balance
+  attr_reader :name
+
+  ACCOUNTS = []
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    save
+  end
+
+  def deposit(amount)
+    #needs to run only if valid? returns true
+    @balance += amount
+    display_balance
+  end
+
+  def display_balance
+    "Your Balance is $#{self.balance}."
+  end
+
+  def valid?
+    # binding.pry
+    ACCOUNTS.include?(self) && (status == "open") && balance > 0
+    #this doesn't evaluate the transaction itself
+    # #both_valid? iterates, sends one acct at a time here
+    #only evaluates each individual account, 
+    #called by Transfer.both_valid?
+    #checks: 1. Account exits, 2. Account status == "open" 3. Account balance >= 0
+
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+  def self.all
+    ACCOUNTS
+  end
+
+  def save
+    ACCOUNTS << self
+  end
+
 end
+
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,60 @@
+require 'pry'
+
 class Transfer
-  # code here
+  attr_accessor :status
+  attr_reader :amount, :sender, :receiver
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def both_valid?
+    # binding.pry
+    sender.valid? && receiver.valid? ? true : self.status = "rejected"
+
+    #works just fine like this, so long as "sender" & "receiver"
+    #are objects of class BankAcount
+    #meaning if the Transfer's initialized with just a string, 
+    #this will reject it, b/c it's not checking for that string.
+    #will need to update #valid? to check for string 
+
+  end
+
+  def execute_transaction
+    if self.both_valid? && (self.status = "pending")
+      binding.pry
+      sender.deposit(-amount)
+      receiver.deposit(amount)
+      self.status = "complete"
+    else
+      reject._transfer
+    end
+    binding.pry
+    #This takes the result of #both_valid? and: 
+      #1. checks that transfer.status == "pending"
+      #2. debits sender's acct
+      #3. credits receiver's acct
+      #4. changes transfer.status to either "executed" or "rejected"
+    #
+
+  end
+
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+  def reverse_transaction
+    if self.status == "completed"
+      receiver.deposit(-amount)
+      sender.deposit(amount)
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+
 end
