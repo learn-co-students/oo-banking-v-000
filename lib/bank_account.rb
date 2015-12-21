c317bd1f7e682b1c06e636321d00aa8f01efd869
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2015-12-21 21:27:05 +0000

    complete.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
   # code here
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize (name)
+      @name=name
+      @balance=1000
+      @status="open"
+  end
+  
+  def deposit(money)
+     @balance=@balance+money 
+  end
+  
+  def display_balance
+      return "Your Balance is $#{@balance}."
+  end
+  
+  def valid?
+     if @status=="open" && @balance >0 
+         return true
+     else
+         return false
+     end
+  end
+  
+  def close_account
+     @status="closed"
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,45 @@
 class Transfer
   # code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize (sender, receiver, amount)
+      @sender=sender
+      @receiver=receiver
+      @amount=amount
+      @status="pending"
+  end
+  
+  def both_valid?
+     if sender.valid? && receiver.valid?
+         return true
+     else
+         return false
+     end
+  end
+  
+  def execute_transaction
+  if @status=="pending" && sender.balance > @amount
+  sender.balance-=@amount
+  receiver.deposit(@amount)
+  @status="complete"
+  else
+      reject_transfer
+  end
+  end
+  
+  def reject_transfer
+      if sender.balance < @amount 
+          @status="rejected"
+          return "Transaction rejected. Please check your account balance."
+        
+      end
+  end
+  
+  def reverse_transfer
+      if @status=="complete"
+          sender.deposit(@amount)
+          receiver.balance-=@amount
+          @status="reversed"
+      end
+  end
 end
