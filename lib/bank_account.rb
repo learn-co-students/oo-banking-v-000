c8c48e32f5cdce7d55993d634ebeda06beabe8d8
lizdenhup <lizdenhup@gmail.com>
2015-12-22 21:48:44 -0600

    finished oo banking lab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+  attr_reader :name 
+  attr_accessor :balance, :status 
   # code here
+
+  def initialize(name, balance = 1000, status = "open")
+    @name = name 
+    @balance = balance
+    @status = status 
+  end 
+
+  def deposit(amount)
+    if amount != nil
+    @balance = @balance += amount 
+  end
+
+  def display_balance
+    "Your Balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0 
+  end 
+  end
+
+  def close_account
+    @status = "closed"
+  end 
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
+
 class Transfer
   # code here
+  attr_accessor :amount, :sender, :receiver, :status, :last_transaction_amount
+
+  def initialize(sender, receiver, status= "pending", amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+  end
+
+  def both_valid?
+    sender = self.sender
+    receiver = self.receiver 
+    sender.valid? && receiver.valid? 
+  end
+
+  def execute_transaction
+    if self.both_valid? && self.sender.balance >= amount && self.status == "pending"
+      self.receiver.balance += amount 
+      self.sender.balance -= amount
+      self.last_transaction_amount = amount 
+      self.status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end 
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+        self.sender.balance += self.last_transaction_amount
+        self.receiver.balance -= self.last_transaction_amount
+        self.status = "reversed"
+    end 
+  end 
+
 end
