8cb82cdd9f684a74c6c25bcfc7d0550c241c0b4d
Tsundu <osmentdan@gmail.com>
2016-01-02 11:23:26 -0500

    added objects with relationships

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,40 @@
 class BankAccount
-  # code here
+  attr_accessor :balance, :status, :amount
+  attr_reader :name 
+
+  def initialize(name)
+    @name = name 
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(num)
+    @balance += num 
+  end 
+
+  def display_balance 
+     "Your Balance is $#{@balance}."  
+  end
+  
+  def valid?
+    if self.balance > 0 && self.status == 'open'
+      true
+    else
+      false 
+    end 
+  end
+
+  def close_account
+    self.status = 'closed'
+    
+  end
+
+
+
+
+
+
+
+
+
 end
