ea2dad071d3d650b8ce1181abcca887947499b8f
mystycs <mystycs@gmail.com>
2016-01-25 21:11:52 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
   # code here
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(cash)
+    @balance += cash
+  end
+
+  def display_balance
+    return "Your Balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
