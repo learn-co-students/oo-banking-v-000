1574b47dfdf2aee6f71376718b1430c01daa0789
shinyrachel <rachel.kory@gmail.com>
2016-02-15 11:11:38 -0500

    transfer initializes

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
-  # code here
+  
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def both_valid?
+  end
+
+  def execute_transaction
+  end
+
+  def reject_transfer
+  end
+
+  def reverse_transfer
+  end
+
+
 end
