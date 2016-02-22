270b548b7e6166e7356e6eccf689f998c01f0bb9
Brian Holland <brian.holland@proatria.com>
2016-02-22 13:03:59 -0600

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,26 @@
 class BankAccount
-  # code here
+  attr_accessor :status, :balance
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(funds)
+    @balance += funds
+  end
+
+  def display_balance
+    return "Your Balance is $#{self.balance}."
+  end
+
+  def valid?
+    return (@status=="open" && @balance>0) ? true : false
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
 class Transfer
-  # code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+
+  def both_valid?
+    #sender.valid? && receiver.valid?
+    return @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if (@status != "pending")
+      return
+    end
+    if !(self.both_valid? && @sender.balance>@amount)
+      self.reject_transfer
+      return "Transaction rejected. Please check your account balance."
+    end
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = "complete"
+  end
+
+  def reject_transfer
+    @status = "rejected"
+  end
+
+  def reverse_transfer
+    if (@status == "complete")
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status="reversed"
+    end
+  end
 end
