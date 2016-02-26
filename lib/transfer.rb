03c7ed211e5329ff176d6739ca970731f3d3ca3d
smithWEBtek <brad@smithwebtek.com>
2016-02-26 11:23:25 -0500

    "Saving this at 21 and 2 left to go"

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,40 +1,55 @@
+require 'pry'
+
 class Transfer
-  attr_reader :amount, :sender, :receiver
-  attr_accessor :status
+attr_accessor :transfer, :sender, :receiver, :status, :amount, :all
+@@all = []
 
   def initialize(sender, receiver, amount)
-    @status = "pending"
+    @transfer = transfer
     @sender = sender
     @receiver = receiver
-    @amount = amount
+    @status = "pending"
+    @amount = 50
+    @@all << self
   end
 
   def both_valid?
-    sender.valid? && receiver.valid?
+    sender.valid?
+    receiver.valid?
   end
 
   def execute_transaction
-    if both_valid? && sender.balance > amount && self.status == "pending"
-      sender.balance -= amount
-      receiver.balance += amount
+    if sender.valid? == true && self.status != "complete"
+      receiver.deposit(amount)
+      sender.debit(amount)
       self.status = "complete"
     else
       reject_transfer
     end
   end
 
+  def reject_transfer
+      sender.valid? != true #|| sender.balance < amount
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+  end
+    #   can reject a transfer if the sender doesn't have a valid account
   def reverse_transfer
-    if both_valid? && receiver.balance > amount && self.status == "complete"
-      receiver.balance -= amount
-      sender.balance += amount
+    if status = "complete"
+      sender.deposit(amount)
+      receiver.debit(amount)
       self.status = "reversed"
-    else
-      reject_transfer
     end
   end
+    #   can reverse a transfer between two accounts 
+    #   it can only reverse executed transfers 
 
-  def reject_transfer
-    self.status = "rejected"
-    "Transaction rejected. Please check your account balance."
-  end
 end
+
+# Transfers start out in a "pending" status. 
+# They can be executed and go to a "complete" state. 
+# They can also go to a "rejected" status. A completed 
+# transfer can also be reversed and go into a "reversed" status.
+
+
+
