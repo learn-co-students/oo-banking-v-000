522e95461ff2968d98f445cb3e9932c5bb9136d0
cernanb <cernanb@gmail.com>
2016-02-29 20:01:58 -0800

    created BankAccount and Transfer classes to interact with each other

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,40 +1,37 @@
 class Transfer
-  attr_reader :amount, :sender, :receiver
-  attr_accessor :status
+  attr_accessor :sender, :receiver, :amount, :status
 
   def initialize(sender, receiver, amount)
-    @status = "pending"
     @sender = sender
     @receiver = receiver
     @amount = amount
+    @status = "pending"
   end
 
   def both_valid?
-    sender.valid? && receiver.valid?
+    @sender.valid? && @receiver.valid?
   end
 
   def execute_transaction
-    if both_valid? && sender.balance > amount && self.status == "pending"
-      sender.balance -= amount
-      receiver.balance += amount
+    if both_valid? && status == "pending" && @sender.balance > amount
+      @receiver.deposit(amount)
+      @sender.deposit(-amount)
       self.status = "complete"
-    else
+    elsif @sender.balance < amount
       reject_transfer
-    end
-  end
-
-  def reverse_transfer
-    if both_valid? && receiver.balance > amount && self.status == "complete"
-      receiver.balance -= amount
-      sender.balance += amount
-      self.status = "reversed"
-    else
-      reject_transfer
-    end
+    end      
   end
 
   def reject_transfer
     self.status = "rejected"
     "Transaction rejected. Please check your account balance."
+  end
+
+  def reverse_transfer
+    if status == "complete"
+      @sender.deposit(amount)
+      @receiver.deposit(-amount)
+      self.status = "reversed"
+    end
   end
 end
