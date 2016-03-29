f54754710a34768f9ac350532ee5321ac343dda3
David Ward <david_ward@Users-MacBook-Pro.local>
2016-03-29 00:04:53 -0400

    transfer completed.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name, balance = 1000, status = "open")
+        @name = name
+        @balance = balance
+        @status = status
+
+    end
+
+    def deposit(amount)
+        @balance += amount
+    end
+
+    def valid?
+        return true if @status == "open" && @balance > 0
+    else
+        false
+    end
+
+    def display_balance
+        "Your Balance is $#{@balance}."
+    end
+
+    def close_account
+        @status = "closed"
+    end
   # code here
 end
