0dbd228d6f73b5b1e4dfeab38fe8838e22a11182
motti770 <mottigottlieb@gmail.com>
2016-04-01 00:54:17 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
   # code here
+  attr_accessor :sender, :receiver, :status
+  def initialize(sender, receiver, status="pending")
+        @sender = sender
+        @receiver = receiver
+        @status = status
+  end
 end
