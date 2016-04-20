20203e22aa308c5ce6199457f142efdd1936f888
karuna24s <karuna24s@gmail.com>
2016-04-20 10:19:34 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if self.status == "complete"
+    if self.status == "complete" && both_valid? && receiver.balance > amount
       sender.balance += amount
       receiver.balance -= amount
       self.status = "reversed"
