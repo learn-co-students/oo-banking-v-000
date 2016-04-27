f331a6dd405b02358c7f300579447d0d1e98c46a
hangloos <ericloos00@gmail.com>
2016-04-27 13:37:39 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,58 +2,46 @@ class Transfer
 
 attr_accessor :sender, :receiver, :status, :amount
 
-def initialize(sender, receiver, amount)
-@sender = sender
-@receiver = receiver
-@amount = amount
-@status = "pending"
-end
+    def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    end
 
-def both_valid?
+    def both_valid?
 
-if self.sender.valid? == true && self.receiver.valid? == true
-  true
-else
-  false
-end
-end
-
-
-def execute_transaction
-
-if self.both_valid? == true && sender.balance > amount && self.status != "complete"
-self.receiver.deposit(amount)
-sender.balance = sender.balance - amount
-self.status = "complete"  
-
-else
-self.status = "rejected"
-return "Transaction rejected. Please check your account balance."
-end
-end
+    if self.sender.valid? == true && self.receiver.valid? == true
+        true
+    else
+        false
+    end
+    end
 
 
-def reverse_transfer
+    def execute_transaction
 
-if self.status == "complete" 
-  self.receiver.balance = self.receiver.balance - amount
-  sender.balance = sender.balance + amount
-  self.status = "reversed"
-else
-end
-end
+    if  self.both_valid? == true && sender.balance > amount && self.status != "complete"
+        self.receiver.deposit(amount)
+        sender.balance = sender.balance - amount
+        self.status = "complete"  
 
-
-
-##if sender isnt valid then status = "rejected" and the method returns that string
-
+    else
+        self.status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+    end
+    end
 
 
+    def reverse_transfer
 
-
-
-
-
+    if  self.status == "complete" 
+        self.receiver.balance = self.receiver.balance - amount
+        sender.balance = sender.balance + amount
+        self.status = "reversed"
+    else
+    end
+    end
 
 
 
