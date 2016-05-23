5c4b85006fec228420f9c42326ea5f7be8075fc5
wedesignstudios <deelden@gmail.com>
2016-05-23 16:57:12 -0500

    added all class methods

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
 class Transfer
-  # your code here
+
+  attr_reader :sender, :receiver, :amount, :status
+  
+  @@all = []  
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? == receiver.valid?
+  end
+
+  def execute_transaction
+    if !@@all.include?(self) && sender.valid?
+      sender.balance = sender.balance - amount.to_i
+      receiver.deposit(amount)
+      @status = "complete"
+      @@all << self
+    end
+    if !sender.valid?
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @@all.include?(self)
+      receiver.balance = receiver.balance - amount.to_i
+      sender.deposit(amount)
+      @status = "reversed"
+    end
+  end
+
 end
