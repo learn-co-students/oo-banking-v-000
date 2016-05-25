6c26b214543eb9dcda3f5ac6315fe0dc094937a1
joelbitart1986 <joelbitar1986@gmail.com>
2016-05-25 00:12:59 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
 class Transfer
-  # your code here
+  
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+  
+  def initialize(sender, receiver, amount)
+		@sender = sender
+		@receiver = receiver
+		@amount = amount
+		@status = "pending"
+	end
+	
+	def valid?
+		x = BankAccount.new("Avi")
+		y = BankAccount.new("Amadafdada")
+		x.valid? && y.valid?
+	end
+  
 end
