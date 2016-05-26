52d235dceafa5988cba770adc42eab5ef807efd6
bshap93 <benihana858@gmail.com>
2016-05-25 20:12:09 -0600

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,42 @@
+require 'pry'
 class BankAccount
+
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def broke
+    if balance <= 0
+      @broke = true
+    else
+      @broke = false
+    end
+  end
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @broke = false
+  end
+
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if self.status == "open" && self.broke == false
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
