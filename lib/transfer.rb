5fbec18a424ec0064d1dcad1894878697fe2db8f
Sandy Hogan <svern99@aol.com>
2016-06-07 22:03:45 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,10 +27,9 @@ class Transfer
     def reverse_transfer
       if @status == "complete"
         sender.balance += amount
-        receiver.balance -+ amount
+        receiver.balance -= amount
         @status = "reversed"
       end
     end
-
 
 end
