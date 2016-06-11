791b9619c24946e12e2ccdd64ab1570a9ad33921
bjacooper <bjacooper@gmail.com>
2016-06-11 14:52:22 +0100

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  @@all = []
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def valid?
+    BankAccount.all.detect {|account| account.name == sender.name}.valid? && BankAccount.all.detect {|account| account.name == receiver.name}.valid?
+  end
 end
