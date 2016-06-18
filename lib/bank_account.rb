19d37ce2ccb02b53c9ce8354f19bfccb9719f461
Jennifer Pazos <jmp521@gmail.com>
2016-06-18 02:18:24 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,42 @@
+
 class BankAccount
+  attr_accessor  :balance, :status
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @@all = [@name, @balance, @status]
+binding.pry
+  end
+
+  def name=(name)
+    raise AssociationTypeMismatchError
+    @name = name
+  end
+
+  def deposit(new_deposit)
+    @balance += new_deposit
+  end
+
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @balance > 0 && @status == "open"
+      return true
+
+    else
+      return false
+    end
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount
+  attr_reader :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    if BankAccount.valid? == true
+      binding.pry
+    end
+  end
+
+
+
 end
