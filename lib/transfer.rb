9e2faa2c19ae27d5d2d6457ce0768d6f82f5a004
Lenn Hypolite <lenntriniman@hotmail.com>
2016-06-28 20:22:53 +0000

    completed first conditional that assures transfer will only happen onc

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
 
-  attr_accessor :sender, :receiver, :amount, :balance
-  attr_reader :status, :bank_account
+  attr_accessor :status
+  attr_reader :balance, :sender, :receiver, :amount
 
   def initialize(sender, receiver, amount)
     @amount = 50
@@ -18,10 +18,12 @@ class Transfer
   end
 
   def execute_transaction
-    bank_account = BankAccount.new(balance)
-    @receiver.balance = @receiver.balance + @amount
-    @sender.balance = @sender.balance - @amount
-    @status = "complete"
+    if @status == "pending"
+      bank_account = BankAccount.new(balance)
+      @receiver.balance = @receiver.balance + @amount
+      @sender.balance = @sender.balance - @amount
+      @status = "complete"
+    end
     #  1.times(execute_transaction)
     # binding.pry
   end
