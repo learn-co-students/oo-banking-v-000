2a3c7974f619069443ee3fd6f6fa996e596d86dc
Lenn Hypolite <lenntriniman@hotmail.com>
2016-06-28 19:19:26 +0000

    initial commit

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
+require "pry"
 class BankAccount
+
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(bank_account)
+    @bank_account = bank_account
+    @name = "Avi"
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit)
+    @balance = @balance.to_i + deposit.to_i
+    @balance
+  end
+
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  def valid?
+    if @balance > 0 && status == "open"
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :balance
+  attr_reader :status, :bank_account
+
+  def initialize(sender, receiver, amount)
+    @amount = 50
+    @receiver = receiver
+    @sender = sender
+    @status = "pending"
+  end
+
+  def valid?
+    valid = BankAccount.new(sender) #establish a collaberation between the two classes to check that both accounts are valid
+    valid.valid?
+    @sender.valid?
+    @receiver.valid?
+  end
+
+  def execute_transaction
+    bank_account = BankAccount.new(balance)
+    @receiver.balance = @receiver.balance + @amount
+    @sender.balance = @sender.balance - @amount
+    @status = "complete"
+    #  1.times(execute_transaction)
+    # binding.pry
+  end
+
 end
