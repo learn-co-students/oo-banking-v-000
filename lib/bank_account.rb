22cbe99b4be9fcf889c91030794e7306d16c6a2c
Keith Wellman <keith.wellman@icloud.com>
2016-07-06 00:20:51 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
 
+  @@accounts = []
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @@accounts << self
+  end
+
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    self.balance > 0 && self.status == "open"
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,45 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    self.receiver.valid? && self.sender.valid?
+  end
+
+  def execute_transaction
+    if self.status != "complete"
+      if  ((self.sender.balance - self.amount) > 0) && ((self.receiver.balance + self.amount) > 0)
+        self.receiver.balance += self.amount
+        self.sender.balance -= self.amount
+        self.status = "complete"
+      else
+        self.bad_transfer
+      end
+    else
+      self.bad_transfer
+    end
+  end
+
+  def bad_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      if  ((self.sender.balance + self.amount) > 0) && ((self.receiver.balance - self.amount) > 0)
+        self.receiver.balance -= self.amount
+        self.sender.balance += self.amount
+        self.status = "reversed"
+      else
+        self.bad_transfer
+      end
+    end
+  end
 end
