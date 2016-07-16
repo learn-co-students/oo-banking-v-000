880060a22302c910f3dc29cbb75de7866d2f8051
Elias Ventura <eliasventura23@gmail.com>
2016-07-16 22:43:45 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
 class BankAccount
 
+attr_accessor :balance, :status
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def balance
+    @balance
+  end
+
+  def status
+    @status
+  end
+
+  def deposit(money)
+    self.balance += money
+  end
+
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  def close_account
+  self.status = "closed"
+end
+
+def valid?
+  balance > 0 && status == "open"
+end
+
 end
