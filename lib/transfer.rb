9aeedbd42b9148e190c0a8a1edc2bfce41d94dd7
William Jones <enquiries@forestfed.com>
2016-08-07 13:08:49 +0000

    more collaborating objects

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,43 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  @@sender = ""
+  @@receiver = ""
+  @@all = []
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver= receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    if @sender.valid? and @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if @sender.valid? and @sender.balance >= @amount
+      if @status == "pending"
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete"
+      end
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
+
 end
