9838a75d70cb842a848a50d8b6e850c11efa4401
eleanorgraham <eleanorcjkgraham@gmail.com>
2016-08-19 10:01:01 -0400

    Gringotts is functioning

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    self.sender = sender
+    self.receiver = receiver
+    self.amount = amount
+    self.status = "pending"
+  end
+
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if self.valid? == false
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    elsif sender.balance < amount
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    elsif (self.valid? == true) && (self.status != "complete")
+      sender.balance = sender.balance - amount
+      receiver.balance = receiver.balance + amount
+      self.status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance = sender.balance + amount
+      receiver.balance = receiver.balance - amount
+      self.status = "reversed"
+    end
+  end
 end
