ed3306f37533a31dd0e17d343280c6253d610386
Emmanuel Owusu <e_owusu@live.com>
2016-11-26 20:52:21 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @account = 1000
+  end
+
+  def deposit(amount)
+   @balance += amount
+  end
+
+  def valid?
+   @status == "open" && @balance > 0
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
 end
