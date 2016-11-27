a99c3c7ce2dbc7e5fd799335a32650879a21801e
Pamela Bortnick <pbortnick@gmail.com>
2016-11-27 21:25:39 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    balance = @balance += amount
+    balance
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if @balance > 0 && @status == "open"
+      true
+    else
+      false
+    end
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 
 end
