885d28f0fbfd653c5ca6fb2362d1135f2774aebf
dwyn <dwayne.harmon@gmail.com>
2016-12-17 22:46:05 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
 class Transfer
-  # your code here
+  attr_accessor :bank_account, :sender, :status, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @amount = amount
+    @sender = sender
+    @status = "pending"
+    @receiver = receiver
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if sender.status != "closed" || amount < sender.balance
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    end
+  end
 end
