f272eb875c014be8c7fbfadfad882b384685d88a
dwyn <dwayne.harmon@gmail.com>
2016-12-18 02:54:46 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,10 +13,27 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.status != "closed" || amount < sender.balance
+    if valid? && sender.balance > amount && self.status == "pending"
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
+    else
+      reject_transfer
+    end
+  end
+
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
+    else
+      reject_transfer
     end
   end
 end
