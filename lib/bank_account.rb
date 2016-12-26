be2ac5d3d847b37d8c57eeb2873a323559bc46e6
Alexis Roman <alexismmroman21@gmail.com>
2016-12-26 20:40:48 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,42 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def name
+    @name.dup.freeze
+
+  end
+
+  def deposit(money)
+
+    @balance += money
+
+  end
+
+  def display_balance
+
+    "Your balance is $#{@balance}."
+
+  end
+
+  def valid?
+    if @balance== 0 || @status == "closed"
+      false
+    else
+      true
+    end
+
+  end
+
+  def close_account
+    @status="closed"
+
+  end
+
 end
