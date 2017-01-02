b78da5ecdf970c6c879201c0adb72f523d12489e
Ryan Evans <ryanwevans@icloud.com>
2017-01-01 23:12:29 -0800

    add interacting classes

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? && @sender.balance >= @amount
+  end
+
+  def execute_transaction
+    if self.valid? && @status == "pending"
+        @sender.balance -= amount
+        @receiver.balance += amount
+        @status = "complete"
+    else !self.valid?
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.balance -= amount
+      @sender.balance += amount
+      @status = "reversed"
+    end
+  end
+
 end
