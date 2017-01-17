a601a8d9b567ad34497465a9defd48a9ec5130e7
ryanpmcmahon <ryan@mcmahon.work>
2017-01-16 21:58:57 -0500

    started bank_account and transfer classes

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    self.sender.valid? && self.receiver.valid?
+  end
+
+  def execute_transaction
+    if !valid?
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    elsif status == "pending"
+      receiver.deposit(amount)
+      sender.deposit(amount * -1)
+      self.status = "complete"
+    end
+    binding.pry
+  end
 end
