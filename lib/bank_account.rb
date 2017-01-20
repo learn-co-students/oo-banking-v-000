5bccbc31a48f8036de4d730a70a157c75ededbb4
Terry Thompson <terry.lyn.thompson@gmail.com>
2017-01-20 02:46:29 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
+attr_accessor  :balance, :status
+attr_reader :name
+
+def initialize(name)
+  @name = name
+  @name.freeze
+  @balance = balance
+  @balance = 1000
+  @status = status
+  @status = "open"
+end# of initialize
+
+
+def deposit(amount)
+@balance += amount
+end# of depost
+
+
+def display_balance
+    "Your balance is $#{@balance}."
+end# of display_balance
+
+
+def valid?
+  @status == "open" && @balance > 0 ? true : false
+end# of valid?
+
+
+def close_account
+  self.status = "closed"
+end#close_account
 
 end
