36ec4bfbfd315fa08ba531e01c69409f93d8f1bc
Kevin Adkins <kevinladkins@gmail.com>
2017-01-28 19:18:44 +0000

    built Transfer execute_transaction; 9 tests passing

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,5 +13,17 @@ class Transfer
   def valid?
     sender.valid? && receiver.valid?
   end
-  
+
+  def execute_transaction
+    if self.valid?
+      receiver.deposit(@amount)
+      sender.balance = sender.balance - @amount
+      @amount = 0
+      @status = "complete"
+    else
+       "Transaction rejected. Please check your account balance."
+       @status = "rejected"
+    end
+  end
+
 end
