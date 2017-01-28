916eb6dae1ef16f34663f594572888f7095654de
Kevin Adkins <kevinladkins@gmail.com>
2017-01-28 18:35:19 +0000

    built BankAccount initialize and display_balance methods

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,15 @@
 class BankAccount
 
+  attr_reader :name, :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def display_balance
+    @balance
+  end
+
 end
