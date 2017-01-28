fc595ef03b490456486fd5c30510a91f1d86e1bb
Kevin Adkins <kevinladkins@gmail.com>
2017-01-28 19:01:11 +0000

    built Transfer attr_accessor and initialize but tests not passing

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
 end
