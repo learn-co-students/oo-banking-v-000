f9aade9351c2eebc198948b4003912058b99c5d4
Andrea Toston <tostonar@gmail.com>
2017-02-02 03:29:13 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,19 +12,19 @@ class Transfer
   end
 
   def valid?
-
-    @sender.valid? && @receiver.valid?
+    if @sender.valid? && @receiver.valid?
+      @sender.balance > @amount
+    end
   end
 
   def execute_transaction
-
     if valid? && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
     else
-      "Transaction rejected. Please check your account balance."
       @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 
