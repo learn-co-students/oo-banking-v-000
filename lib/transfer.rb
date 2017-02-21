847a89fffec2dce848c94847c8722881f2c14e92
Daniel Lempesis <danlrcn@hotmail.com>
2017-02-21 05:13:18 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Transfer
-  # your code here
+
+  attr_accessor :balance, :status, :amount
+  attr_reader :receiver, :sender
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid? #...sweet. So I got to call this from banking_spec.rb
+  end
+
+
+  def execute_transaction()
+    #can only happen once
+    if @status != "pending" || sender.balance < @amount
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    else
+      sender.balance -= @amount
+      receiver.balance += @amount
+      @status = "complete"
+    end
+  end
+
+
+  def reverse_transfer()
+    if self.status == "complete"     #can only reverse executed transfers
+      sender.balance += @amount
+      receiver.balance -= @amount
+      self.status = "reversed"
+    end
+  end
 end
