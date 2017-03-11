a96a848305ba68e0432dfe862d3b63b00e866a9e
Isis Tejeda <isis@isistejeda.com>
2017-03-11 15:07:49 -0600

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+
+
+  def initialize(name)
+    @transfers = []
+    @name = name
+    @balance = 1000
+    @status = 'open'
+
+  end
+
+
+  def deposit(money)
+    @balance += money
+  end
+
+  def display_balance
+     "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @balance > 0 && @status == 'open'
+  end
+
+  def close_account
+    @status = 'closed'
+  end
+
 
 end
