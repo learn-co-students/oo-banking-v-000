0bf14b126b799197a136ef0fb10d7df1775a913e
Seth Alexander <seth.a.alexander@gmail.com>
2017-03-15 02:45:53 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,7 +17,7 @@ class BankAccount
   end
 
   def valid?
-    @balance > 0 && @status == "open" ? true : false
+    @balance > 0 && @status == "open"
   end
 
   def close_account
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
 
   def valid?
-    @sender.valid? && @receiver.valid? ? true : false
+    @sender.valid? && @receiver.valid?
   end
 
   def execute_transaction
