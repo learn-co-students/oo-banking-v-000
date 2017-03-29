a16ad4c2ac2bbba70012ba85cac2fad45962de99
jamcelr <jamcelr@hotmail.com>
2017-03-29 17:16:51 -0400

    Refactored code

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
-  
+
   def initialize(name)
     @name = name
     @balance = 1000
@@ -13,16 +13,16 @@ class BankAccount
   end
 
   def display_balance
-    "Your Balance is $#{@balance}."
+    "Your Balance is $#{self.balance}."
   end
 
   def valid?
-    @balance > 0 && @status == "open"? true : false
+    @balance > 0 && @status == "open" ? true : false
   end
 
   def close_account
     @balance = 0
     @status = "closed"
   end
-  
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,14 +9,14 @@ class Transfer
   end
 
   def both_valid?
-    sender.valid? && receiver.valid? ? true : false
+    sender.valid? && receiver.valid?
   end
 
   def execute_transaction
-    unless @status == "complete"
-      if both_valid? && sender.balance > @amount
-        @sender.balance -= amount
-        @receiver.balance += amount
+    unless status == "complete"
+      if both_valid? && sender.balance > amount
+        sender.balance -= amount
+        receiver.deposit(amount)
         @status = "complete"
       else
         reject_transfer
@@ -25,16 +25,16 @@ class Transfer
   end
 
   def reject_transfer
-    if sender.valid? == false || sender.balance < @amount
+    if sender.valid? == false || sender.balance < amount
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
 
   def reverse_transfer
-    if status == "complete"
-      @sender.balance += @amount
-      @receiver.balance -= @amount
+    if @status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
       @status = "reversed"
     end
   end
