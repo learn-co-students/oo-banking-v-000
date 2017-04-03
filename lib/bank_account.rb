457a51ff6533bebf3ddfc42c1b472d0dcd3859c7
William Barela <william.barela.dev@gmail.com>
2017-04-03 17:28:54 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+  attr_writer :balance, :status           # doing this because the BDD tests require it, but not good practice.
+  attr_reader :name, :balance, :status
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit_amt)
+    self.balance += deposit_amt if deposit_amt >=0
+  end
+
+  def withdraw(withdraw_amt)
+    (withdraw_amt >= 0 && withdraw_amt < self.balance) ? (self.balance -= withdraw_amt) : ("Transaction rejected. Please check your account balance.")
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    (self.balance > 0 && self.status == "open") ? true : false
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 end
