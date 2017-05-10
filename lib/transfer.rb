59e6a782c59e51d0c8f4f004ed7a1434797c5422
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-05-10 21:13:02 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if execute_transaction
+    if execute_transaction @@ self.status == "complete"
       @receiver.balance -= @amount
       @sender.balance += @amount
       self.status = "reversed"
