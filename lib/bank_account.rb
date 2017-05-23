5910321b8708a544fd2a0c8e8ef9329d06fc63a2
Noah Summers <summers.noah@gmail.com>
2017-05-23 04:44:31 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
 class BankAccount
 
+  attr_reader :name
+  
+  attr_accessor :balance, :status
+
+  def initialize(owner_name)
+    @name = owner_name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
 end
