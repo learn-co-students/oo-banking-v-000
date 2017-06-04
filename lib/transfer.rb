97a253ed92fd90af29bdd8f21505cb5202e4bff2
ctab33 <ctab33@me.com>
2017-06-04 13:07:47 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,12 +15,12 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.balance > amount && self.status == "pending"
+    if sender.balance > amount && status == "pending"
       sender.balance -= amount
       receiver.balance += amount
-      self.status = "complete"
+      status = "complete"
     else
-      self.status = "rejected"
+      status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
