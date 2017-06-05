723dacfc02093ae88ed8503038f387413407a9b8
Pia Kim <pkim2320@gmail.com>
2017-06-05 16:42:41 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
 
+attr_accessor :balance, :status
+attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit)
+    @balance += deposit
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    status == "open" && balance > 0
+
+
+  end
+
+
+
+  def close_account
+    self.status = "closed"
+  end
 end
