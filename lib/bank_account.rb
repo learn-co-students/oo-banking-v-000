f900f44cf6bdb6bc4770f88dc2908777231b3724
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-06-12 18:11:00 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,40 @@
+require 'pry'
 class BankAccount
+  attr_accessor :name, :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def status=(change_status)
+    @status = change_status
+  end
+
+  def name=(new_name)
+    raise ChangeNameError if new_name != name
+    @name = new_name
+  end
+
+  def deposit(integer)
+    @balance+= integer
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = 'closed'
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,44 @@
+require 'pry'
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount = 50)
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @amount = amount
+  end
+
+  def valid?
+    if sender.valid? && receiver.valid? == true
+      true
+    end
+  end
+
+  def execute_transaction
+    #binding.pry
+    if @status == 'pending' && @sender.valid? && @sender.balance > self.amount
+      #binding.pry
+      new_balance_receiver = receiver.balance + amount
+      new_balance_sender = sender.balance - amount
+      receiver.balance = new_balance_receiver
+      sender.balance = new_balance_sender
+      @status = 'complete'
+    else
+      @status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == 'complete'
+      sender_reversed_balance = sender.balance + amount
+      receiver_reversed_balance = receiver.balance - amount
+      sender.balance = sender_reversed_balance
+      receiver.balance = receiver_reversed_balance
+      @status = 'reversed'
+    end 
+  end
+
 end
