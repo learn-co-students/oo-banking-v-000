578c11036807d8eb2b049885f3cc5f5621f5eb9b
Sydney Chun <schun08@gmail.com>
2017-07-04 18:05:53 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
+# first attempt 6/27/17
+# class BankAccount
+#   attr_accessor :balance, :status
+#   attr_reader :name
+#
+#   def initialize(name)
+#     @name = name
+#     @balance = 1000
+#     @status = "open"
+#   end
+#
+#   def deposit(amount)
+#     self.balance += amount
+#   end
+#
+#   def display_balance
+#     "Your balance is $#{self.balance}."
+#   end
+#
+#   def valid?
+#     self.balance > 0 && self.status == "open"
+#   end
+#
+#   def close_account
+#     self.status = "closed"
+#   end
+#
+# end
+
+# second attempt 7/4/17
 class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
@@ -8,8 +38,8 @@ class BankAccount
     @status = "open"
   end
 
-  def deposit(amount)
-    self.balance += amount
+  def deposit(money)
+    self.balance += money
   end
 
   def display_balance
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,21 +1,60 @@
-require 'pry'
+# first attempt 6/27/17
+# require 'pry'
+# class Transfer
+#   attr_accessor :sender, :receiver, :amount, :status
+#
+#   def initialize(sender,receiver,amount)
+#     @sender = sender
+#     @receiver = receiver
+#     @status = "pending"
+#     @amount = amount
+#   end
+#
+#   def valid?
+#     self.sender.valid? && self.receiver.valid?
+#   end
+#
+#   def execute_transaction
+#     #binding.pry
+#     if self.status == "pending" && valid? && sender.balance > amount
+#       sender.balance -= amount
+#       receiver.balance += amount
+#       self.status = "complete"
+#     else
+#       self.status = "rejected"
+#       "Transaction rejected. Please check your account balance."
+#     end
+#   end
+#
+#   def reverse_transfer
+#     if self.status == "complete"
+#       sender.balance += amount
+#       receiver.balance -= amount
+#       self.status = "reversed"
+#     end
+#   end
+# end
+
+# second attempt 7/4/17
 class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
 
   def initialize(sender,receiver,amount)
     @sender = sender
     @receiver = receiver
-    @status = "pending"
     @amount = amount
+    @status = "pending"
   end
 
   def valid?
-    self.sender.valid? && self.receiver.valid?
+    sender.valid? && receiver.valid?
   end
 
   def execute_transaction
-    #binding.pry
-    if self.status == "pending" && valid? && sender.balance > amount
+    # why do you need self.status but not self.receiver or self.sender when you are defining the #execute_transfer method?
+      # you don't need self.receiver or self.sender here because they already refer to instances of the BankAccount class
+    if valid? && self.status == "pending" && sender.balance > amount
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
@@ -32,4 +71,5 @@ class Transfer
       self.status = "reversed"
     end
   end
+
 end
