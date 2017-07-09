ca2649921973ae6b8c7f4018de4dfae41b8643c6
Robert Peckham <peckham.rob@gmail.com>
2017-07-09 23:12:30 +0000

    finished BankAccount class

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
 class BankAccount
+  attr_accessor :account, :balance, :status
+  attr_reader :name
 
+  def initialize(name, *account)
+    @name = name
+    @account = account
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(ammount)
+    @balance += ammount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0 ? true : false
+  end
+
+  def close_account
+    @status = "closed"    
+  end
+
+  
 end
