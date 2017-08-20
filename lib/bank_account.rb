0e0889b4d0878dc4eb870982e0d0add3eaf238b1
Karen Berba <kberba@gmail.com>
2017-08-20 00:13:31 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,56 @@
-class BankAccount
+# BankAccount
+#   #initialize
+#     can initialize a Bank Account
+#     initializes with a name
+#     always initializes with balance of 1000
+#     always initializes with a status of 'open'
+#     can't change its name
+#   #deposit
+#     can deposit money into its account
+#   #display_balance
+#     "Your balance is $1000."
+#     can display its balance
+#   #valid?
+#     is valid with an open status and a balance greater than 0
+#   #close_account
+#     can close its account
 
+
+require 'pry'
+
+class BankAccount
+  attr_accessor :account, :balance, :status
+  attr_reader :name, :deposit, :display_balance, :valid
+
+  def initialize(account)
+    @account = account
+    @name = account
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(money)
+    @balance = @balance + money
+  end
+
+  def display_balance
+    p "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    #binding.pry
+    if self.status == "open" && self.balance > 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+#
+#
+#
+#
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,62 @@
+# Transfer
+#    initialize
+#      can initialize a Transfer
+#      initializes with a sender
+#      initializes with a receiver
+#      always initializes with a status of 'pending'
+#      initializes with a transfer amount
+#    #valid?
+#      can check that both accounts are valid
+#      calls on the sender and reciever's #valid? methods
+#    #execute_transaction
+#      can execute a successful transaction between two accounts
+#      each transfer can only happen once
+#      rejects a transfer if the sender doesn't have a valid account
+#    #reverse_transfer
+#      can reverse a transfer between two accounts
+#      it can only reverse executed transfers
+
 class Transfer
-  # your code here
+  attr_accessor :transfer, :sender, :receiver, :status, :amount, :valid, :bank_account, :account, :balance
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    if self.amount > 0
+      if sender.valid? && receiver.valid?
+        return true
+      end
+    else
+      return false
+    end
+  end
+
+  def execute_transaction
+    if sender.balance < amount
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+    if self.status == "pending"
+      sender.balance = sender.balance - amount
+      receiver.balance = receiver.balance + amount
+      self.status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance = sender.balance + amount
+      receiver.balance = receiver.balance - amount
+      self.status = "reversed"
+    end
+  end
+#
+#
+#
+#
 end
