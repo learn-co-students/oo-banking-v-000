6c7635f252f6c9db4cad510492a1c7e6e9409593
Susan Fowler <infopro94@gmail.com>
2017-09-09 02:16:45 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,40 @@
 class BankAccount
 
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def balance
+    @balance = 1000
+  end
+
+  def status
+    @status = "open"
+  end
+
+  def deposit #can deposit money into its acct
+    @deposit
+  end
+
+  def display_balance #can display its balance
+    "Your balance is $1000."
+  end
+
+  def valid? #is valid w/open status & balance > than 0
+    if status == "open" and balance >= 0
+      valid
+      else
+        false
+      end
+    end
+
+    def close_account
+      @status = "closed"
+    end
+
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,45 @@
 class Transfer
-  # your code here
+
+  # attr_accessor :name, :status
+
+  def initialize(transfer)
+    @transfer = transfer
+  end
+
+  # def transfer
+  #   @transfer
+  # end
+
+  def sender
+    @sender = sender
+  end
+
+  def receiver
+    @receiver = receiver
+  end
+
+  def status
+    @status = "pending"
+  end
+
+  def transfer_amount
+    @transfer_amount
+  end
+
+  def valid?
+    #both accounts are valid
+    #calls on the sender and receivers valid? methods
+  end
+
+  def execute_transaction
+    #can execute a successful transaction betw two accounts
+    #each transfer can only happen once
+    #rejects a transfer if the sender doesn't have a valid accounts
+  end
+
+  def reverse_transfer
+    #can reverse a transfer betw two accounts
+    #it can only reverse executed Transfers
+  end
+
 end
