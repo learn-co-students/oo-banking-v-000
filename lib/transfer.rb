9e93a3faf2ac6df7c5a56e0f7cdd4399728994ae
matos89 <matos89@gmail.com>
2017-09-19 22:19:33 -0700

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,6 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
-      binding.pry
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
