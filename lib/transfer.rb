c446213c23b3ee2bac1bb8fd5461b6016b2958f7
matos89 <matos89@gmail.com>
2017-09-19 22:18:26 -0700

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? && status != "complete"
+    if valid? && status != "complete"
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
