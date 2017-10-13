01fd7158ced0ec7aa636f41ab824d563ca1ad17e
zolkocarbon <eathena@hotmail.com>
2017-10-13 12:13:45 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
 
-end
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit_amount)
+    @balance += deposit_amount
+  end
+
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @balance == 0 || @status == "closed" ? false : true
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
+end # class closer
