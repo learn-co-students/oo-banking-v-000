0147a90a8d8227bde5eda4e6e03ed26cd6ba7885
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2017-10-15 16:21:47 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,28 @@
+require "pry"
+
 class Transfer
-  # your code here
+  attr_accessor :name, :amount, :status, :sender, :receiver
+
+  def initialize(name, receiver, amount)
+    @name = name
+    @receiver = receiver
+    @amount = 50
+    @status = "pending"
+    @sender = name
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    # binding.pry
+    if valid? == true  #once the Transfer.valid? is true,
+      @sender.balance - @receiver.deposit(amount)
+      @receiver.balance = @receiver.balance + (@sender.balance - @receiver.deposit(amount)) #add the transfer amount (@amount) to the receiver account
+      @sender.balance
+    #need to have a deposit amount increase the balance in the receiver account
+    end
+  end
+
 end
