195d0316582582a21db871a4e5598c7d43853b36
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-30 21:39:41 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
 class Transfer
   # your code here
+  attr_reader
+  attr_accessor
+
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    self.sender.valid? && self.receiver.valid?
+  end
+
+  def execute_transaction
+    if self.valid? && self.status == "pending"
+      self.sender -= amount
+      self.receiver += amount
+      self.status = "completed"
+  end
+
 end
