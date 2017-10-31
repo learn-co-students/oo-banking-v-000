57cfc011d58fab996048286c046cde23597dfc5e
Anastasia  Barkett <anastasia.barkett@gmail.com>
2017-10-31 04:15:41 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,6 @@
 class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
 
   def initialize(sender, receiver, amount)
     @sender = sender
