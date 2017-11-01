283d5fcf6a0fa7235820935200773787bc300be9
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-01 23:29:23 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
 class BankAccount
+  attr_accessor :balance, :status, :name
+
+  def initialize(name)
+    @balance = 1000
+    @status = "open"
+    @name = name
+  end
+
+  def name=(name)
+    raise ArgumentError.new("Can't change the name of a bank account!")
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @balance > 0 && @status == "open" ? true : false
+  end
+
+  def close_account
+    @balance = 0
+    @status = "closed"
+  end
 
 end
