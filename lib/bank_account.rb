ce71b6e28f520ffd833aea8307e0aa7a3fe691e6
Christopher DiStasio <github email address>
2017-11-14 11:34:25 -0600

    first commit

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class BankAccount
+
+    attr_accessor :balance, :status
+    attr_reader :name, :deposit
+
+    def initialize(name) 
+        @name = name
+        @balance = 1000
+        @status = "open"
+        raise ImproperChangeError if name != name        
+    end
+
+    def deposit(deposit)
+        @deposit = deposit
+        @balance = balance + deposit
+    end
+
+    def display_balance
+        return "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        if @status == "open" && @balance > 0
+            true
+        else
+            false    
+        end
+    end
+
+    def close_account
+        @status = "closed"
+    end
 
 end
