c5321b1756ff68e5de410c40874c6db523490c0a
Emily Zhao <tong.zhao.92@gmail.com>
2017-11-21 22:32:46 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
+attr_accessor :balance, :status, :transfer
+attr_reader :name
+@@all = []
+
+  def initialize(account_name)
+    @name = account_name
+    @@all<<self
+    @balance = 1000
+    @status = "open"
+    @transfer = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def display_balance
+    return "Your balance is $#{self.balance}."
+  end
+
+  def deposit(amount)
+    @balance = @balance + amount
+  end
+
+  def valid?
+    @status=="open" && @balance>0
+  end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,54 @@
+require 'pry'
 class Transfer
-  # your code here
+attr_accessor :sender, :receiver, :status, :amount
+
+@@all = []
+
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+    BankAccount.all.find{|account|account == sender}.transfer<<self
+  end
+
+  def valid?
+    if BankAccount.all.include?(self)
+      return true
+    elsif BankAccount.all.find{|account|account == @sender}.valid? && BankAccount.all.find{|account|account == @receiver}.valid?
+      return true
+    else
+      return false
+    end
+  end
+
+  def sender_account_rec
+    BankAccount.all.find{|account|account == @sender}
+  end
+
+  def execute_transaction
+    if self.valid?
+      if @amount>self.sender_account_rec.balance
+        @status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+      elsif @status == "pending"
+        @receiver.balance += @amount
+        @sender.balance -= @amount
+        @status = "complete"
+      end
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end
+  end
+
+
 end
