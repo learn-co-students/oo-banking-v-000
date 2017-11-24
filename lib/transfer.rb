0395601b20780c3f4a2b705b05b7e091e0372f3a
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-24 01:47:16 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  def initialize(sender, receiver, amount = 50)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+  end
+
+  def execute_transaction
+
+  end
+
+  def reverse_transfer
+  end
+
 end
