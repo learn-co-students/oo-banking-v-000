45b18c7f5e96763fbcaa3d9bf18158f0671ffd2a
charityyoungblood <youngblood.charity@gmail.com>
2017-12-05 10:28:13 -0800

    added comments for structure of deposit method; added placeholder for deposit method

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -4,10 +4,15 @@ class BankAccount
 # create initialize method w/argument (name) 
   def initialize(name)
     @name = name
+  # add balance of $1000 to initialize method
     @balance = 1000
+  # initialize with a status of open (instance variable)
     @status = "open"
   end 
-# add balance of $1000 to initialize method
-# initialize with a status of open (instance variable)
-# create attr_reader for name - so that name cannot be changed 
+  
+# create deposit instance method
+  # - this method should change the balance to the amount of the deposit + the original balance
+  def deposit 
+    
+  end 
 end
