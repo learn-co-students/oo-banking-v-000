c857ee7b750b94a520da44bfd322db4d93f3c26a
charityyoungblood <youngblood.charity@gmail.com>
2017-12-05 10:21:03 -0800

    added comments on how to structure code

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,6 @@
 class BankAccount
-
+# create initialize method w/argument (name) 
+# add balance of $1000 to initialize method
+# initialize with a status of open (instance variable)
+# create attr_reader for name - so that name cannot be changed 
 end
