59c3af2f4476e5f1c0e0e0c4dc58d0e52dcfbf5b
charityyoungblood <youngblood.charity@gmail.com>
2017-12-05 10:34:54 -0800

    added comments for how to structure display_balance method

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,7 +13,10 @@ class BankAccount
 # create deposit instance method
   # - this method should change the balance to the amount of the deposit + the original balance
   def deposit(amount)
- 
     @balance += amount
   end 
+  
+  # create display_balance instance method 
+    # - this method should return the string "Your balance is $#{bankaccount instance.balance}."
+  
 end
