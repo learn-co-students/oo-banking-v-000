ee2d3955515fee4ccef40272653cd8a265d665bc
charityyoungblood <youngblood.charity@gmail.com>
2017-12-05 11:38:19 -0800

    added attr_accessor for sender and receiver; added both to initialize method

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,14 +2,15 @@ require 'pry'
 
 class Transfer
   # create initialize method 
-  
+  attr_accessor :sender, :receiver
   def initialize(sender, receiver, status)
-    binding.pry
+    
     # - method should initialize with the following
-    # - sender
-   
-    #@sender =
-    # - receiver 
+    # - sender = equal to the BankAccount instance
+    @sender = sender
+    # - receiver = equal to other BankAccount instance
+    @receiver = receiver
+    
     # - status of "pending"
     # - transfer amount = 50
   end 
