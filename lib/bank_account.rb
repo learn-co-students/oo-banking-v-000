f183d5845edd5a38315377633adb1f0b363a108e
charityyoungblood <youngblood.charity@gmail.com>
2017-12-05 10:22:19 -0800

    added placeholder for initialize method; attr_reader for name

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,10 @@
 class BankAccount
+# create attr_reader for name - so that name cannot be changed 
+  attr_reader :name
 # create initialize method w/argument (name) 
+  def initialize(name)
+    @name = name
+  end 
 # add balance of $1000 to initialize method
 # initialize with a status of open (instance variable)
 # create attr_reader for name - so that name cannot be changed 
