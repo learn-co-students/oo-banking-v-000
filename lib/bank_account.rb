cfd157a597383523d61160f184549a766d50cbbc
Sharmina Akhtar <sakht318@gmail.com>
2017-12-07 14:40:08 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,38 +1,44 @@
 class BankAccount
-  attr_accessor :status, :balance, :transfer, :deposit
+  attr_accessor :status, :transfer, :balance
   attr_reader :name
   @@all =[]
 
-  def initialize(name, status = "open")
+  def initialize(name)
     @name = name
     @status = "open"
     @balance = 1000
-    @deposit > 0
   end
 
-  def deposit
-    deposit > 0
-    balance + deposit = deposit
+
+
+  def deposit(amount)
+    @balance += amount 
   end
 
-  def display_balance(name)
-    "Your balance is $#{name.balance}"
+
+  def display_balance
+    "Your balance is $#{balance}."
   end
 
-  def valid?(name)
+  def valid?
+    #is valid with an open status and a balance greater than 0
     #@broke = self.new(name)
-    #@broke.balance = 0
+    #balance = 0
+    #@broke = balance
     #@closed = self.new(name)
-    #@closed.status = "closed"
-    if status = "open" && balance > 0
+    #@status = "closed"
+    #@closed = @status
+    #balance = balance > 0
+    if @status == 'open' && @balance > 0
       true
     else
       false
     end
   end
 
-  def close_account(name)
-    name.status = "closed"
+  def close_account
+      @status = "closed"
   end
+
 
 end
