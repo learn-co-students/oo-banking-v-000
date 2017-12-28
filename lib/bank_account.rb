e179c232c630a1f526fcef74a415d325c6394f0e
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2017-12-28 23:23:27 +0000

    ☕ SOLUTION

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
-
+#==========================================
+  # INSTANCE ATTRIBUTES
+#==========================================
+attr_accessor :balance, :status
+attr_reader :name
+#==========================================  
+  # INSTANCE
+#==========================================
+  def initialize(name)
+    @name = name
+    self.balance = 1000
+    self.status = "open"
+  end
+  
+  def deposit(amt)
+    self.balance += amt
+  end
+  
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+  
+  def valid?
+    self.status == "open" && self.balance > 0? true:false
+  end
+  
+  def close_account
+    self.status = "closed"
+  end
+#==========================================
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,50 @@
 class Transfer
-  # your code here
+#=========================================================
+  # INSTANCE ATTRIBUTES
+#=========================================================
+attr_accessor :sender, :receiver, :amount, :status
+#=========================================================  
+  # INSTANCE
+#=========================================================
+  def initialize(sender, receiver, amount)
+    self.sender = sender
+    self.receiver = receiver
+    self.amount = amount
+    self.status = "pending"
+  end
+  
+  def valid?
+    self.sender.valid? && self.receiver.valid?? true:false
+  end
+
+  def execute_transaction
+    self.uniq_and_valid_amount ? self.accept : self.reject
+  end
+  
+  def reverse_transfer
+    if self.status == "complete" then self.reverse end
+  end
+  
+#=======================HELPERS============================  
+  def uniq_and_valid_amount
+    self.status == "pending" and self.sender.balance >= self.amount
+  end
+  
+  def accept
+    self.sender.balance -= self.amount
+    self.receiver.balance += self.amount
+    self.status = "complete"
+  end
+  
+  def reject
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+  
+  def reverse
+    self.sender.balance += self.amount
+    self.receiver.balance -= self.amount
+    self.status = "reversed"
+  end
+#========================================================== 
 end
