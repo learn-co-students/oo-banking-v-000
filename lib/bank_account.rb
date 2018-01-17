56ddc08db81c8249235be494e3c4eddec2a91bd6
K Logic <kellyayers411@gmail.com>
2018-01-17 21:40:09 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,13 @@
 class BankAccount
-
+  
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize (name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end
+    
+    
 end
