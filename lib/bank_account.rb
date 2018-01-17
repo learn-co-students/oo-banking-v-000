0830d445173bd81711b6424244883f7dd7569ac7
K Logic <kellyayers411@gmail.com>
2018-01-17 23:13:42 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,6 +8,22 @@ class BankAccount
     @balance = 1000
     @status = 'open'
   end
+  
+  def deposit(amount)
+    @balance += amount
+  end
+  
+  def display_balance
+    "Your balance is $#{balance}." #double quotes required for string interpolation
+  end
+  
+  def valid?
+    status == 'open' && balance > 0
+  end
+  
+  def close_account
+    @status = 'closed'
+  end
     
     
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize (namefrom, nameto, amount)
+   @sender = namefrom
+   @receiver = nameto
+   @amount = amount
+   @status = 'pending'
+  end
+  
+  def valid? 
+    sender.valid? && receiver.valid?
+  end
+  
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = 'complete'
+    else
+      self.status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+  def reverse_transfer
+    if self.status == 'complete'
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = 'reversed'
+    else
+    end
+  end
+
 end
