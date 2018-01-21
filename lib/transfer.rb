3b9121de6476ed2504ecc8d19d3b471c08aa4b6e
jljardon <github email address>
2018-01-21 16:42:56 -0500

    add #reject_transfer
    Added #reject_transfer to simplify #reverse_transer and #execute_transaction.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,7 @@ class Transfer
          receiver.balance += @amount
          @status = 'complete'
       else
-         @status = 'rejected'
-         'Transaction rejected. Please check your account balance.'
+         reject_transfer
       end
    end
 
@@ -28,6 +27,13 @@ class Transfer
          sender.balance += @amount
          receiver.balance -= @amount
          @status = 'reversed'
+      else
+         reject_transfer
       end
+   end
+
+   def reject_transfer
+      @status = 'rejected'
+      'Transaction rejected. Please check your account balance.'
    end
 end
