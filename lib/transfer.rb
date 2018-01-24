43f1c414944cd35b4a23a959aded52570d336193
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-24 16:16:13 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,17 +27,28 @@ attr_accessor :sender, :receiver, :status, :amount
       sender.balance -= amount
       self.status = "complete"
     else
-      self.status = "rejected"
-     "Transaction rejected. Please check your account balance."
+      reject_transfer
+      #self.status = "rejected"
+     #"Transaction rejected. Please check your account balance."
+     #build the above logic into it's own method
     end
 
   end
+
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+
 
   def reverse_transfer
     if self.status == "complete"
       receiver.balance -= amount
       sender.balance += amount
       self.status = "reversed"
+    else
+      reject_transfer
     end
   end
 
