2a1b0c605fd216f11019c8b958558b70cd293057
jtorre20 <jtorres1989@gmail.com>
2018-02-02 17:26:30 -0500

    completed 15 out of 20

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,18 @@
+require 'pry'
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, status = "pending", amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+
+  def valid?
+    #binding.pry
+    sender.valid? && receiver.valid? ? true : false
+  end
+  
 end
