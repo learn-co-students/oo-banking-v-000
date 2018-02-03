41c8b0763efb92253319f9dc9a267e992a8e596b
jtorre20 <jtorres1989@gmail.com>
2018-02-03 07:42:20 -0500

    done

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,9 +10,35 @@ class Transfer
     @amount = amount
   end
 
-  def valid?
-    #binding.pry
+  def valid? #need to verify that both the sender and 
+    #receiver are valid accounts. send/rece are part of a BankAccount
+    # so valid method can be used on them
     sender.valid? && receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+    
+    if @status == "pending"
+      receiver.balance += self.amount
+      sender.balance -= self.amount
+      @status = "complete" 
+    end
+    
+    if sender.valid? != true
+      @status = "rejected"
+      if @status == "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+  end
+
+  def reverse_transfer 
+    if @status == "complete"
+      sender.balance += self.amount
+      receiver.balance -= self.amount
+      #binding.pry
+      @status = "reversed"
+    end
   end
   
 end
