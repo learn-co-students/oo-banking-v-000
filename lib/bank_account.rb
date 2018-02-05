bf1a2a1803091e0b0c54e766905da2075a1b4619
Ron Sala <ron.sala@gmail.com>
2018-02-05 01:06:26 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,6 +13,14 @@ class BankAccount
   end
 
   def display_balance
-    "Your balance is $#{avi.balance}."
+    "Your balance is $#{balance}."
+  end
+
+  def valid?
+    status == "open" && balance > 0
+  end
+
+  def close_account
+    self.status = "closed"
   end
 end
