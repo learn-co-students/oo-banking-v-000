926b80e487d257934f7713c59a32c9a73fbc9158
Ron Sala <ron.sala@gmail.com>
2018-02-05 02:09:02 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,21 @@
 class Transfer
+  attr_accessor :status, :amount
+  attr_reader :sender, :receiver
 
-  def initialize(transferer, transferee, transfer_amount)
-    
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    sender.balance -= amount
+    receiver.balance += amount
+    @status = "complete"
   end
 end
