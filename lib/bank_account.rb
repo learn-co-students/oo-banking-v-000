47da5a2967c043dd97944aae2a79a31d14381d53
Eden Mesfun <emesfun@gmail.com>
2018-02-15 14:51:58 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class BankAccount
+
+attr_accessor :balance, :status
+
+attr_reader :name
+
+    def initialize(name)
+      @name=name
+      @balance = 1000
+      @status = "open"
+    end
+
+    def deposit(deposit)
+      @balance += deposit
+    end
+
+    def display_balance
+      return "Your balance is $#{@balance}."
+    end
+
+    def valid?
+      self.balance >= 1 && self.status == "open"
+    end
+
+    def close_account
+      self.status = "closed"
+    end
 
 end
