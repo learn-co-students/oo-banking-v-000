529f07f8739a32916c92d15ac0a240589a8f21fc
Jay Bednar <ironviralmedia@gmail.com>
2018-02-17 20:36:42 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,19 +9,19 @@ class BankAccount
   end
 
   def deposit(amount)
-    @balance += amount
+    self.balance += amount
   end
 
   def display_balance
-    "Your balance is $#{@balance}."
+    "Your balance is $#{self.balance}."
   end
 
   def valid?
-    @balance > 0 && @status == 'open'
+    self.balance > 0 && self.status == 'open'
   end
 
   def close_account
-    @status = 'closed'
+    self.status = 'closed'
   end
 
 end
