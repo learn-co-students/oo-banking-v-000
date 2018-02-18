0e2467edd5e5c58a1b777620d8d1bef4a0281e2e
Brent  Ransom <sadumedusa@gmail.com>
2018-02-18 14:08:03 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -4,7 +4,27 @@ class BankAccount
 
   def initialize(name)
     @name = name
-    @balance = '1000'
+    @balance = 1000
     @status = 'open'
+  end
+
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def display_balance
+  "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if status == 'open' && balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = 'closed'
   end
 end
