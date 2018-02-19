29b46307be2f383bb94b930d71bb67ebd46b063f
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-19 17:50:38 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,43 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status
+  attr_reader :amount
+  
+  @@execute_transaction = false
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+  
+  def execute_transaction
+    if valid? && @sender.balance > @amount
+      @sender.balance  = @sender.balance - @amount
+      @receiver.deposit(amount)
+      @status = "complete"
+      @@execute_transaction = true
+      define_singleton_method(:execute_transaction) {}
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+  def reverse_transfer
+    if @@execute_transaction == true
+      @receiver.balance  = @receiver.balance - @amount
+      @sender.deposit(amount)
+      @status = "reversed"
+      @@execute_transaction = false
+    end
+  end
 end
