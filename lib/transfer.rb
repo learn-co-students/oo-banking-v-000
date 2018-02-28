f556f93666210bf051fd773543ecd1c183fec86d
Jayon Kim <jpkim921@gmail.com>
2018-02-28 20:45:24 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,7 +8,27 @@ class Transfer
 		@receiver = receiver
 		@status = "pending"
 		@amount = amount
+	end
+
+	def valid?
+		sender.valid? && receiver.valid?
+	end
+	
+  def execute_transaction
+    if self.status == "pending"
+    
+      sender.balance = sender.balance - amount
+			receiver.balance = receiver.balance + amount
+    
+      if self.valid?
+        self.status = "complete"
+		  else
+			  self.status = "rejected"
+			  return "Transaction rejected. Please check your account balance."
+		  end
+    end		
 		
 	end
+
 
 end
