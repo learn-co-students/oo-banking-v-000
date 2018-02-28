f67ed5077629eb7fa555e3889b6cbeddfda8df0a
Veronica Kim <vero.a.kim@gmail.com>
2018-02-28 04:36:27 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
 
+attr_accessor :bank_account, :balance, :status
+attr_reader :name, :broke, :closed
+
+  def initialize(name)
+    @bank_account = bank_account
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(num)
+    @balance = balance + num
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+     if @status = "closed" || self == @broke
+       return false
+     elsif @status = "open" || @balance > 0
+       return true
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,14 @@
 class Transfer
-  # your code here
+
+attr_accessor :transfer, :sender, :receiver, :status, :name
+
+  def initialize(sender, transfer, receiver)
+    @transfer = transfer
+    @sender = sender
+    @status = "pending"
+
+  end
+
+
+
 end
