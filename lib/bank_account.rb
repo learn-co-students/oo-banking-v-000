89df56197ea5aaa62dd5ec15d49a3546d7b97400
Jayon Kim <jpkim921@gmail.com>
2018-02-28 19:40:46 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,7 +6,18 @@ class BankAccount
 		@name = name
 		@balance = 1000
 		@status = "open"
-		
+	end
+	
+	def deposit(dollars)
+		self.balance = balance + dollars
+	end
+	
+	def display_balance
+		return "Your balance is $#{balance}."
+	end
+	
+	def valid?
+		status == "open" && balance > 0 ? true : false
 	end
 
 end
