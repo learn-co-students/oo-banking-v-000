d127d7f4c6ddb57a8d5d20c360fe3d513bacebbe
Robert W <robwash@gmail.com>
2018-03-09 22:39:12 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
     end
 
     def execute_transaction
-      if self.status == "pending" && self.sender.balance >= self.amount
+      if valid? && self.status == "pending" && self.sender.balance >= self.amount
         self.sender.balance -= self.amount
         self.receiver.balance += self.amount
         self.status = "complete"
@@ -25,7 +25,7 @@ class Transfer
     end
 
     def reverse_transfer
-      if self.status == "complete"
+      if valid? && self.status == "complete"
         self.sender.balance += self.amount
         self.receiver.balance -= self.amount
         self.status = "reversed"
