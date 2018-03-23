ead8b360382ed3076bf99857ceb447e034f3f559
Jonathan Luk <jluk87@gmail.com>
2018-03-23 18:19:28 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,7 @@
 class BankAccount
-  attr_reader :name
+
   attr_accessor :balance, :status
+  attr_reader :name
 
   def initialize(name)
     @name = name
@@ -9,20 +10,23 @@ class BankAccount
   end
 
   def deposit(amount)
-    self.balance+=amount
+    self.balance += amount
   end
 
   def display_balance
-    "Your balance is $#{balance}."
+    "Your balance is $#{self.balance}."
   end
 
   def valid?
-    self.status == 'open' && balance > 0
+    self.status == 'open' && self.balance > 0
   end
 
   def close_account
     self.status = 'closed'
   end
 
-
 end
+
+jon = BankAccount.new('jon')
+jon.close_account
+puts jon.valid?
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,50 +2,42 @@ require 'pry'
 require_relative '../lib/bank_account.rb'
 
 class Transfer
+
   attr_accessor :status
-  attr_reader :amount, :sender, :receiver
-  @@statuses = ['pending', 'complete', 'rejected', 'reversed']
+  attr_reader :sender, :receiver, :amount
+
   def initialize(sender, receiver, amount)
+    @status = 'pending'
+    @amount = amount
     @sender = sender
     @receiver = receiver
-    @status = self.class.statuses[0]
-    @amount = amount
-  end
-
-  def self.statuses
-    @@statuses
   end
 
   def valid?
-    sender.valid? && receiver.valid?
-  end
-
-  def change_status(new_status)
-    self.status = new_status
+    self.sender.valid? && self.receiver.valid?
   end
 
   def execute_transaction
-    if self.valid? && self.status == 'pending' && sender.balance >= self.amount
-      sender.balance -= self.amount
-      receiver.balance +=self.amount
-      change_status(self.class.statuses[1])
-      self
+    if self.valid? && self.status == 'pending' #valid account and tranfer is in pending status
+      if self.sender.balance >= amount #sender has enough money?
+        self.sender.balance -= self.amount #ok, do it
+        self.receiver.balance += self.amount
+        self.status = 'complete'
+      else
+        self.status = 'rejected' #reject, seller doesn't have money
+        "Transaction rejected. Please check your account balance."
+      end
     else
-      change_status(self.class.statuses[2])
-      "Transaction rejected. Please check your account balance."
+      self.status = 'rejected' #either one of the accounts are not valid or transfer is not in pending status
     end
   end
 
   def reverse_transfer
-    if self.status == self.class.statuses[1]
-      reversal = Transfer.new(self.receiver, self.sender, self.amount)
-      reversal.execute_transaction
-      change_status(self.class.statuses[3])
+    if self.valid? && self.status == 'complete' #accounts are valid and transfer was executed before?
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = 'reversed'
     end
   end
 
 end
-
-account1 = BankAccount.new('jon')
-account2 = BankAccount.new('stacy')
-transfer = Transfer.new(account1, account2, 50)
