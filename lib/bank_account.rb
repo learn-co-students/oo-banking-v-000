9f183b41eb6839c0c3f6cb84de911414629ae854
Aderianna T Williams <aderiannaw@yahoo.com>
2018-04-02 18:02:05 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,15 @@
 class BankAccount
+  attr_accessor :status, :balance
+  attr_reader :name
 
+
+def initialize(name)
+  @name = name
+  @status = 'open'
+  @balance = 1000
+end
+
+def deposit 
+  
+end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :status 
+  attr_reader  :sender, :receiver, :amount
+  
+ def initialize(sender, receiver, amount)
+   @sender = sender
+   @receiver = receiver
+   @status = 'pending'
+   @amount = amount
+end
 end
