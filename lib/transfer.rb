84679f928f138eb0e2d937c1548a59e3a48b2148
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-04-03 00:08:22 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 class Transfer
   # your code here
+  attr_reader :sender, :receiver
+  attr_accessor :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+
+  end
 end
