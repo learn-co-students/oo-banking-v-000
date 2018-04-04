17601d68fea2530aeb8969b5b7c15e1655a5b9c6
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-04-04 02:59:38 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,6 +13,17 @@ class Transfer
   end
 
   def valid?
+    @sender.valid? && @receiver.valid?
+  end
 
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else sender.balance <= 0 && self.status == "open"
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
   end
 end
