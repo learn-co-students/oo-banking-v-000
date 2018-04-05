f04f9d361f85592a6604434f80146a22c9df5dc2
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-05 00:46:39 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,26 @@
 class BankAccount
+  attr_reader :name, :balance, :status
+  attr_writer :status, :balance
 
+  def initialize (name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit (amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
