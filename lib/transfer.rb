58c846e0c2c01a45ca39778cbac42737819af0b1
Aderianna T Williams <aderiannaw@yahoo.com>
2018-04-06 05:42:38 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,13 +3,23 @@ class Transfer
   attr_accessor :status 
   attr_reader  :sender, :receiver, :amount
   
- def initialize(sender, receiver, amount)
-   @sender = sender
-   @receiver = receiver
-   @status = 'pending'
-   @amount = amount
-end
+   def initialize(sender, receiver, amount)
+     @sender = sender
+     @receiver = receiver
+     @status = 'pending'
+     @amount = amount
+  end
+  
+   def valid?
+     
+  end
+  
+  def execute_transaction
+    
+  end
+  
+  def reverse_transfer
+    
+  end
 
- def valid?
-   
 end
