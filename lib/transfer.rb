20e7949c78cf85a55d9d2a5e80542eeb59749fa5
Aderianna T Williams <aderiannaw@yahoo.com>
2018-04-07 02:34:20 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,15 +11,35 @@ class Transfer
   end
   
    def valid?
-     
+     if receiver.valid? && sender.valid? then true
+    end
   end
+
+  def reject_transfer
+    @status = "rejected"
+  "Transaction rejected. Please check your account balance."
+    end
   
   def execute_transaction
-    
+     if valid? && sender.balance > amount && status =="pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      @status ="complete"
+    else
+      reject_transfer
   end
+  end
+
   
   def reverse_transfer
-    
+     if valid? && receiver.balance > amount && @status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      @status = "reversed"
+    else
+      reject_transfer
+    end
   end
 
 end
+
