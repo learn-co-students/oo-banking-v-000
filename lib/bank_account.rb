03f037f27a7c96719e5325ecd11911d9c29502d7
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-10 22:52:51 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name, :deposit
 
+
+  def initialize(name)
+    @name = name.capitalize
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
