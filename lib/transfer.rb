22ccb60ac52124cade5526fe28cf268b7c7ead43
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-20 00:19:12 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,11 @@
 class Transfer
-  attr_accessor :sender, :receiver, :status
+  attr_accessor :sender, :receiver, :status, :amount
   
   def initialize(sender, receiver, status)
     @sender = sender
     @receiver = receiver
+    @amount = 50
     @status = "pending"
   end
+  
 end
