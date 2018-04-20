df2cf92a494d0a837c64a2b38225c0b7252380c5
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-20 00:06:03 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,11 @@
-class BankAccount
+class BankAccount 
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status
+  
+  def initialize(sender, receiver, status)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
 end
