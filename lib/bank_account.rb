af462fee2afae6687c06604507cd2db39f2c855f
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-22 17:42:21 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,5 +7,26 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
+  
+  def deposit(deposit_amount)
+    @balance = deposit_amount + @balance
+    @balance
+  end
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+  
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 
 end
