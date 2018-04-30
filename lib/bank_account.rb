8b355163e029004e61fe93b0e439d8a31a68edef
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-30 04:41:30 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
 
+  attr_reader :name
+  attr_accessor  :status, :balance
+
+
+  def initialize(name)
+   @name = name
+   @status = "open"
+   @balance = 1000
+
+  end
+
+
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  def deposit(deposit_amount)
+   self.balance += deposit_amount
+ end
+
+ def valid?
+   status == "open" && balance > 0
+   end
+
+
+def close_account
+  self.status = "closed"
+end
+
+
 end
