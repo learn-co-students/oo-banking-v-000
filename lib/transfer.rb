6301223d5606f711d1f0e445c076b33c7c48f183
Anu Khambete <anukhambete@gmail.com>
2018-05-03 18:30:53 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,41 @@ class Transfer
   
   def execute_transaction
     
+    if @status == "pending"
+      
+      if self.valid? == true
+        
+        if @sender.balance > @amount
+          @sender.balance = @sender.balance - @amount
+          @receiver.balance = @receiver.balance + @amount
+          @status = "complete"
+        else
+          @status = "rejected"
+          "Transaction rejected. Please check your account balance."
+        end      
+      else
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+    
   end
+  
+  def reverse_transfer
+    
+    if @status == "complete"
+      
+      @sender.balance = @sender.balance + @amount
+      
+      @receiver.balance = @receiver.balance - @amount
+      
+      @status = "reversed"
+    
+    end
+      
+  end
+  
+  
+  
+  
   
 end
