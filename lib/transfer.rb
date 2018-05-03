0eef06df0c9eccf8051355bfc8abc29008254e1e
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-05-03 00:50:38 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,12 +15,28 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? && sender.balance > amount
-      status = "complete"
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
     else
-      status = "rejected"
-      puts "Transaction rejected. Please check your account balance."
+      reject_transfer
     end
+  end
+
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
   end
 
 end
