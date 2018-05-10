587107a5c37b9054224dca9294135deac543f6f9
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-10 20:58:46 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
 end
