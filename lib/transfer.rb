d5450cf44a2f0cda8764dcceba47890c38090ce0
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-10 22:57:41 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,4 +11,30 @@ class Transfer
     self.sender.valid? && self.receiver.valid? && self.status == "pending"
   end
 
+  def execute_transaction
+    if status == "pending"
+      self.sender.balance -= amount
+      self.receiver.balance += amount
+      self.status = "complete"
+    elsif self.sender.valid? == "false"
+      self.status == "rejected"
+    end
+    "Transaction rejected. Please check your account balance."    #binding.pry
+  end
+
+  def reverse_transfer
+    if status == "complete"
+      self.sender.balance += amount
+      self.receiver.balance -= amount
+      self.status = "reversed"
+    end
+  end
+
 end
+=begin
+
+elsif  self.sender.valid? == false
+  "Transaction rejected. Please check your account balance."
+  self.status = "rejected"
+end
+=end
