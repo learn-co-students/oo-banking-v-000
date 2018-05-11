2909c87e8f0961451c710a9ec30eccf5414fd00d
Kitty Davies <kittyfdavies@gmail.com>
2018-05-11 20:12:00 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 class Transfer
-  # your code here
+
+  attr_reader
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    BankAccount.valid?
+  end
+
+  def execute_transaction
+
+  end
+
+  def reverse_transfer
+
+  end
+
 end
