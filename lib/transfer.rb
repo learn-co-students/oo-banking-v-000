809c45194fbd4c76f4d73b74d7ad0391934ff014
Kitty Davies <kittyfdavies@gmail.com>
2018-05-12 20:52:06 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,15 +13,28 @@ class Transfer
   end
 
   def valid?
-    BankAccount.valid?
+    receiver.valid? && sender.valid?
   end
 
   def execute_transaction
-
+    if @status == "pending"
+      sender.balance = sender.balance - @amount
+      receiver.balance = receiver.balance + @amount
+      if !receiver.valid? || !sender.valid?
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      else
+        @status = "complete"
+      end
+    end
   end
 
   def reverse_transfer
-
+    if @status == "complete"
+      sender.balance = sender.balance + @amount
+      receiver.balance = receiver.balance - @amount
+      @status = "reversed"
+    end
   end
 
 end
