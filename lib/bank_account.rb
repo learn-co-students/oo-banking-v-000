72b90bb88e280deb1bd271177db352f3476f4446
Brent Bauman <brentbauman86@gmail.com>
2018-05-24 14:56:54 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
+require "pry"
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance = balance + amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @balance > 0 && @status == "open"
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
