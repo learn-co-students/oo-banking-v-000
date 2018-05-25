75a9e0498da435d0b2aae47ee347a25e4de6f428
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-25 23:44:42 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,16 @@
 
 class Transfer
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    self.status = "pending"
+    self.sender = sender
+    self.receiver = receiver
+    self.amount = amount
+  end
+  
+  def valid?
+    
+  end
   
 end
