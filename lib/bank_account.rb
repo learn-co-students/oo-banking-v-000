ad61598585e4adaf664417ca707872708feda2cb
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-31 00:06:45 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,40 @@
 class BankAccount
+  @@all = []
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @@all << self
+  end
+
+  def deposit(amount)
+    @balance = @balance + amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+
+    if @balance > 0 && @status == "open"
+      return true
+    else
+      return false
+    end
+
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
+  def self.find_by_name(name)
+    puts @@all.detect{|account| account.name == name}
+
+  end
 end
