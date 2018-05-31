a7522cfcfc0782dca593d8ab651e63b0f8a26a21
Alex Zhang <alexgzhang92@gmail.com>
2018-05-31 04:25:47 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.valid? && @amount <= @sender.balance && @status == "pending"
+    if valid? && @amount <= @sender.balance && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
