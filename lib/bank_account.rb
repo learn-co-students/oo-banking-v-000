4be6939691f3a3a1007cafca27386b3f1261060b
Leticia Molina <letimolina@gmail.com>
2018-06-14 03:49:04 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,4 +8,22 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
+  
+  def deposit(amount)
+    @balance += amount unless amount < 0 
+  end
+  
+  def display_balance 
+    "Your balance is $#{balance}."
+  end
+  
+  def valid?
+    self.status == "open" && self.balance > 0 
+  end
+  
+  def close_account #(status)
+    @status = "closed"
+  end
+  
+    
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,4 +9,33 @@ class Transfer
     @status = "pending"
   end
   
+  def valid?
+    self.amount > 0 && sender.valid? && receiver.valid?
+  end
+  
+  def execute_transaction
+     if self.valid? && self.status == "pending" && sender.balance > amount
+      sender.balance -= self.amount
+      receiver.balance += self.amount
+      self.status = "complete"
+    else
+      bad_transfer
+   end
+  end
+  
+  def reverse_transfer
+    if self.status == "complete"
+     sender.balance += self.amount
+     receiver.balance -= self.amount
+     self.status = "reversed"
+   end
+ end
+ 
+ def bad_transfer
+   self.status = "rejected"
+   "Transaction rejected. Please check your account balance."
+ end
+      
+    
+  
 end
