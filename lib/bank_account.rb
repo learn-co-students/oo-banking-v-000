781c435448162a304f5b1a2418397ddc1f8efbe2
Laura Messa Travin <laura.travin@gmail.com>
2018-06-26 21:40:35 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
 class BankAccount
-
+  attr_accessor :status, :balance
+  attr_reader :name
+   def initialize(name)
+     @name= name
+     @balance= 1000
+     @status= "open"
+   end
+   def deposit(number)
+     @balance += number
+   end
+   def display_balance
+      return "Your balance is $#{self.balance}."
+   end
+   def valid?
+      if @balance > 0 and @status == "open"
+         return true
+      else
+         return false
+      end
+   end
+   def close_account
+      @status = "closed"
+   end
 end
