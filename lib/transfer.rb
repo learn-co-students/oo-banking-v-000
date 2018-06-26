13ad576953fb135cbc0765031f12b48f4d3e838a
Laura Messa Travin <laura.travin@gmail.com>
2018-06-26 22:52:55 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,18 @@
 class Transfer
-  attr_accessor :sender, :receiver, :status, :amount
+  attr_accessor :sender, :receiver, :status, :amount, :BankAccount
 
   def initialize(sender,receiver,value)
-    @sender= sender
-    @receiver= receiver
+    @sender= sender   #this is an object
+    @receiver= receiver  #this is an object
     @status= "pending"
     @amount= value
+  end
+  def valid?
+    if @sender.valid? and @receiver.valid
+      return true
+    else
+      false
+    end
+
   end
 end
