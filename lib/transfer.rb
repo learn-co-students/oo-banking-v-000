cf9cd46f9a4176869414e32feadc4058c68908e1
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-07-10 15:06:29 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -33,8 +33,8 @@ class Transfer
   
   def reverse_transfer
     if @status == "complete"
-      @sender.deposit( @amount ) 
-      @receiver.deposit( @amount * -1)
+      @sender.deposit(amount) 
+      @receiver.deposit(amount * -1)
       @status = "reversed"
     end
   end
