816e557ab46044be83dfec1397dc6cc56ceefbbf
Christine Xiang <yutingcxiang@gmail.com>
2018-07-16 01:25:49 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,32 +9,28 @@ class Transfer
   end
   
   def valid?
-    if @sender.valid? && @receiver.valid?
-      true
-    else
-      false
-    end
+    @sender.valid? && @receiver.valid?
   end
   
   def execute_transaction
-    if valid? && @sender.balance > @amount && @status == "pending"
-      @sender.balance = @sender.balance - @amount
-      @receiver.balance = @receiver.balance + @amount
+    if valid? && @sender.balance >= @amount && @status == "pending" 
+      @sender.balance -= @amount
+      @receiver.balance += @amount
       @status = "complete"
     else
-      return "Transaction rejected. Please check your account balance."
       @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
   
   def reverse_transfer
     if valid? == true && @status == "complete"
-      @sender.balance = @sender.balance + @amount
-      @receiver.balance = @receiver.balance - @amount
+      @sender.balance += @amount
+      @receiver.balance -= @amount
       @status = "reversed"
     else
-      return "Transaction rejected. Please check your account balance."
       @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
