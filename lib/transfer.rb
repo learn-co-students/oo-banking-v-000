b1cd161f4753cdd54ab61491b9fd2cc9822baf9a
Reno Sorensen <renojames19@gmail.com>
2018-07-18 02:42:32 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,10 +10,19 @@ class Transfer
   end
 
   def valid?
-
+    @sender.valid? && @receiver.valid?
   end
 
-
+  def execute_transaction
+    if @sender.valid? && sender.balance > @amount
+      @sender.deposit(-@amount)
+      @receiver.deposit(@amount)
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
 
 
 
