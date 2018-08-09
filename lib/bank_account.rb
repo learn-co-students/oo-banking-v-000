2af1e8306dae8dc6ee0124887590deef05035895
Azhar Shah <azharshahstl@yahoo.com>
2018-08-09 20:06:14 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,19 @@
 class BankAccount
 
+attr_accessor :status, :balance
+attr_reader :name
+
+  def initialize(name)
+  @name = name
+  @status = "open"
+  @balance = 1000
+  end
+  
+  def deposit(amount)
+    self.balance += amount
+  end
+  
+  def display_balance 
+    return "Your balance is $#{self.balance}."
+  end
 end
