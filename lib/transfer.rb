7cf47c55693ada99463dd1a45244c88bdf293046
Sav Younts <savyounts@gmail.com>
2018-08-10 20:07:21 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if self.status == "complete"
+    if self.status = "complete"
       self.receiver.balance -= self.amount
       self.sender.balance += self.amount
       self.status = "reversed"
