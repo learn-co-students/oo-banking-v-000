d055ec9f23ff0d6710b876eeeae0050466880c54
Amee Covarrubias <ameecova@yahoo.com>
2018-08-12 03:56:07 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,17 +12,26 @@ class Transfer
   end 
   
   def valid?
-    @sender.valid? && @receiver.valid?
+    @sender.valid? && @receiver.valid? 
   end 
   
   def execute_transaction
-    if valid? 
+    if valid? && sender.balance > amount && @status = "pending"
       sender.balance -= amount 
       receiver.balance += amount 
       @status = 'complete'
       #binding.pry
     else 
+      @status = 'rejected'
       "Transaction rejected. Please check your account balance."
+      
     end 
+  end 
+  
+  def reverse_transfer
+    #binding.pry
+    sender.balance += amount
+    receiver.balance -+ amount 
+    @status = 'reversed'
   end 
 end
