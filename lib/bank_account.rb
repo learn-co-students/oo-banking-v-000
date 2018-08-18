c80fa106a1acce6c645971632422e657e68e9d5d
Angie Kassab <anasab12@gmail.com>
2018-08-18 23:51:05 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
-
+  
+  attr_reader :name
+  attr_accessor :balance, :status
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(amount)
+    self.balance += amount # self.balance refers to the instance of the bank account. @balance += amount is okay too, but the first way is the best practice. balance += amount is incorrect because Ruby will think you are creating a new local method with the += sign after it.
+  end
+  
+  def display_balance
+    "Your balance is $#{self.balance}." # or #{balance} is okay because it's calling on the balance getter
+  end
+  
+  def valid?
+    if self.balance > 0 and self.status == "open"
+      true
+    else 
+      false
+    end 
+  end
+  
+  def close_account
+    self.status = "closed"
+  end 
+  
 end
