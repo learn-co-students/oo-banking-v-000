aa94af1eb9aa8928c38d30f7a1299193ecda8f5e
Angie Kassab <anasab12@gmail.com>
2018-08-19 01:26:22 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,37 @@
 class Transfer
   
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?
+    sender.valid? && receiver.valid? # def valid? is using the methods of the BankAccount class
+  end
+  
+  def execute_transaction
+    unless status == "complete" # to make sure the transaction    executes only one time
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    
+      if sender.valid? == false # or !sender.valid?
+        self.status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+      end
+    end
+   end
+   
+   def reverse_transfer
+     if status == "complete" # from the execute_transaction method
+       sender.balance += amount
+       receiver.balance -= amount
+       self.status = "reversed"
+     end 
+   end
+   
 end
