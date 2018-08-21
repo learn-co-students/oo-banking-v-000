bdacc65af51d08e6848e2ae0bf8d5a0d7758d7ba
sharonrieger <sharonarieger@gmail.com>
2018-08-20 20:10:55 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
 class BankAccount
 
+    attr_accessor :balance, :status
+     attr_reader :name
+    
+    def  initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(deposit_amount)
+        self.balance += deposit_amount
+        #or self.balance = self.balance + deposit_amount
+    end
+
+    def display_balance
+      p  "Your balance is $#{balance}."
+    end
+
+    def valid?
+        balance > 0 && status == "open"
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,50 @@
+require 'pry'
+
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+    
+  end
+  
+  #check that both accounts are valid, + if the sender's balance is greater than transfer amount and account statuses are "pending"
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      #subtract the transfer amount from sender's balance
+      sender.balance -= amount
+      #add transfer amount to receiver's balamce
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
+    # binding.pry
+  end
+
+  def reverse_transfer
+    #take the money back from receiver and put back in sender's account
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
 end
