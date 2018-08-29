f7a4ec8568b12b8b7bc65a387fdaef5c77fe9e0a
Rafael Lopez <lpz.raf@gmail.com>
2018-08-29 00:12:44 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,13 @@
 class BankAccount
+  
+  attr_accessor :bank_acct, :name, :balance, :status
+
+def initialize(name)
+  @bank_acct = bank_acct
+  @name = name
+  @balance = 1000
+  @status = "open"
+
+end
 
 end
