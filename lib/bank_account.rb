00999dd228c1a6bdfae1b1ff34672fe89ff82ef4
Rafael Lopez <lpz.raf@gmail.com>
2018-08-29 00:21:06 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,13 +1,17 @@
 class BankAccount
   
-  attr_accessor :bank_acct, :name, :balance, :status
+  attr_accessor :bank_acct, :balance, :status
+  attr_reader :name
 
 def initialize(name)
   @bank_acct = bank_acct
+  @bank_acct = []
   @name = name
   @balance = 1000
   @status = "open"
-
 end
 
+def deposit(money)
+  @bank_acct << money
+end
 end
