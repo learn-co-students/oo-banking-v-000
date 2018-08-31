0c112251d0d500b09d75fdd2f7f6ad008491482d
Rafael Lopez <lpz.raf@gmail.com>
2018-08-31 00:16:35 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,4 +23,22 @@ def execute_transaction
   "Transaction rejected. Please check your account balance."
 end
 end
+def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete" #checks if status is complete. also, the class method for valid? calls valid? on both the receiver and sender
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+end
+
+
+
 end
