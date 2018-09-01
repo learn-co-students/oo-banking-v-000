e8abe3d0a6ad880a8f11311c0ae8ae1918ca0329
Rafael Lopez <lpz.raf@gmail.com>
2018-09-01 01:10:15 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,6 +22,7 @@ def execute_transaction
     self.status = "rejected"
     "Transaction rejected. Please check your account balance."
  end
+ end
 
 def reverse_transfer
   if valid? && receiver.balance > amount && self.status == "complete" 
@@ -37,5 +38,4 @@ def reverse_transfer
     self.status = "rejected"
     "Transaction rejected. Please check your account balance."
   end
-end
 end
