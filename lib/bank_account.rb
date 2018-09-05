91d8cf6a83571bf1bc831977cea5b436da176f27
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-05 03:15:04 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,4 +7,18 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
+  
+  def deposit(money)
+    @balance = @balance + money
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+  
+  def valid?
+    @status == "open" && @balance > 0
+  end
+  
+  def close_
 end
