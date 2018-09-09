7e4388ea1456da93e972ca191e0f8271ac757727
Adam Clayman <adam@clayman.org>
2018-09-09 01:11:00 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if (self.status == "completed")
+    if (self.status == "complete")
       self.sender.balance += self.amount
       self.receiver.balance -= self.amount
       self.status = "reversed"
