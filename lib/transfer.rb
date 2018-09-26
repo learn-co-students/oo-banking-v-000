4fc2c174040492d4234eb0695fd7b1216be62491
Ashley Wong <ashwong04@gmail.com>
2018-09-26 12:41:16 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,13 @@
 class Transfer
   
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
   
-  
-  def initialize
-    
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
   end
   
   
