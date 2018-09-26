2dca4aba115123550404ada9412e18d4d4971ef2
Ashley Wong <ashwong04@gmail.com>
2018-09-26 12:14:50 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,23 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name= name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
+    @balance
+  end
+
+  def display_balance
+    p "Your balance is $#{@balance}."
+  end
+
+
+
 end
