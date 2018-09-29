9451f8bffd4c4cdf2dbf441c8ba1a57baaad7a01
Rachel Wyatt <rw126@evansville.edu>
2018-09-29 21:20:48 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = balance
+    @balance = 1000
+    self.status = "open"
+  end
+
+  def deposit(amount)
+    self.balance = @balance + amount
+  end
+
+  def display_balance
+    return "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if self.status == "open" && self.balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :amount, :status, :balance
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    self.status = "pending"
+  end
+
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+  if self.valid? && receiver.valid? && sender.balance > 0 && self.status == "pending"
+    receiver.balance = receiver.balance + amount
+    sender.balance = sender.balance - amount
+    self.status = "complete"
+  else
+    return "Transaction rejected. Please check your account balance."
+    self.status = "rejected"
+  end
+  end
+
 end
