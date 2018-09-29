d8db9d22c0e6087ce8b6b6d9e5e5f283a45d1d6a
Rachel Wyatt <rw126@evansville.edu>
2018-09-29 21:51:24 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,13 +19,23 @@ class Transfer
   end
 
   def execute_transaction
-  if self.valid? && receiver.valid? && sender.balance > 0 && self.status == "pending"
+  if self.valid? && receiver.valid? && sender.balance > amount && self.status != "complete"
     receiver.balance = receiver.balance + amount
     sender.balance = sender.balance - amount
     self.status = "complete"
   else
-    return "Transaction rejected. Please check your account balance."
     self.status = "rejected"
+    return "Transaction rejected. Please check your account balance."
+  end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+    receiver.balance = receiver.balance - amount
+    sender.balance = sender.balance + amount
+    self.status = "reversed"
+    else
+    "This reverse transfer could not be completed."
   end
   end
 
