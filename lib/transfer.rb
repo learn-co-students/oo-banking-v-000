4933019ee78954e09dee98a7ce61579321eb5e86
Cheryl K <cherylkelson@icloud.com>
2018-10-06 16:09:45 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,37 @@
 class Transfer
-  # your code here
-end
+  
+  attr_accessor :status 
+  attr_reader :sender, :receiver, :amount 
+
+
+  def initialize(receiver)
+    @sender = sender
+    @receiver = receiver 
+    @status = "pending"
+    @transfer_amount = transfer_amount 
+    
+  end 
+  
+  
+  
+  
+  def valid? 
+    
+  end 
+  
+  
+  
+  
+  def execute_transaction
+    
+  end 
+  
+ 
+ 
+  
+  def reverse_transfer 
+    
+  end 
+  
+
+end 
