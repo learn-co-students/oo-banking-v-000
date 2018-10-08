c46e257cb41db52de4aa647b67164a7263006475
Susan Meyers <smmeyers24@gmail.com>
2018-10-08 16:30:34 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,7 +17,18 @@ class BankAccount
     @balance = "#{1000}"
      "Your balance is $#{@balance}."
   end
+  
+  def valid?
+    if@status == "open" && @balance > 0
+      true
+    else
+      @status = "closed"
+      false
+    end
+    end
     
-    
-
-end
+    def close_account
+      @status = "closed"
+    end
+      
+  end
