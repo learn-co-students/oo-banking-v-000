ea604880c9b05ab9a735991ea5dd0d08fb0da570
Susan Meyers <smmeyers24@gmail.com>
2018-10-08 20:06:22 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,49 @@
 class Transfer
   
-  attr_accessor :transfer
+  attr_accessor :transfer, :sender, :receiver, :amount, :status, :balance
   
-  def initialize
-    @transfer = @transfer
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+     @sender.valid?
+     @receiver.valid?
+  end
+  
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending" 
+          sender.balance -= amount 
+          receiver.balance += amount
+          self.status = "complete"
+    else 
+          self.status = "rejected"
+          "Transaction rejected. Please check your account balance."
+           
+    end
+     
   end
 end
+   # but for inside, instead of manually setting them, do you think we can subtract and add the amount to the balances?
+  
+
+    
+      
+        
+      
+      
+      
+     
+   
+ 
+    
+     
+      
+    
+    
+  
+  
+
