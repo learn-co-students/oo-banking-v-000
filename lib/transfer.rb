b0060a79791ccacbe61ddbd1f21d5356805a3774
Susan Meyers <smmeyers24@gmail.com>
2018-10-08 20:26:02 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,32 +16,21 @@ class Transfer
   
   def execute_transaction
     if valid? && sender.balance > amount && self.status == "pending" 
-          sender.balance -= amount 
-          receiver.balance += amount
-          self.status = "complete"
+      sender.balance -= amount 
+      receiver.balance += amount
+      self.status = "complete"
     else 
-          self.status = "rejected"
-          "Transaction rejected. Please check your account balance."
-           
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
-     
   end
 end
-   # but for inside, instead of manually setting them, do you think we can subtract and add the amount to the balances?
+  
+ 
   
 
-    
       
-        
-      
-      
-      
-     
    
- 
-    
-     
-      
     
     
   
