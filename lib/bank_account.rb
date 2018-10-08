8c46539cc45b735b097b846b93a3e01ee12d5c1c
Susan Meyers <smmeyers24@gmail.com>
2018-10-08 15:32:12 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,23 @@
 class BankAccount
+  
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(name)
+    @balance = @balance + 1000
+  end
+  
+  def display_balance
+    @balance = "#{1000}"
+     "Your balance is $#{@balance}."
+  end
+    
+    
 
 end
