90302a2788087adeb6261046d8fcbfbf3b040fc0
Stef Claus <stef.claus@gmail.com>
2018-10-12 02:56:03 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,15 +18,29 @@ class Transfer
   end 
   
   def execute_transaction 
-   
+
+    reject = "Transaction rejected. Please check your account balance."
+    
     if @sender.valid? == false 
-      "Transaction rejected. Please check your account balance."
+      reject  
     elsif @status == "complete"
       nil 
     else 
       @sender.pay(amount)
       @receiver.deposit(amount)
       @status = "complete"
-    end   
+    end 
+    
   end 
-end
+  
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit(amount)
+      @receiver.pay(amount)
+      @status ="reversed"
+    else 
+      nil 
+    end 
+  end 
+  
+end #ends class 
