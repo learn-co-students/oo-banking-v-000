ae46add41aa1b0167a27a2e980fcbbb783688823
Dana Palmer <roiai4@hotmail.com>
2018-10-25 04:17:18 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -10,9 +10,23 @@ class BankAccount
   end
 
   def deposit (amount)
-      new_amount = @balance + amount
+      @balance += amount
   end
 
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
 
+  def valid?
+    if @status == 'open' && @balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = 'closed'
+  end
 
 end
