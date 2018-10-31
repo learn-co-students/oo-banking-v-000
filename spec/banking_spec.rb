b6d856e35d9ed0fb1fe854be19287525771b2b7c
Kenzie Morlock <k.morlock86@gmail.com>
2018-10-31 03:38:39 +0000

    Done.

--- spec/banking_spec.rb
+++ spec/banking_spec.rb
@@ -34,7 +34,7 @@ describe "BankAccount" do
 
   describe '#display_balance' do
     it "can display its balance" do
-      expect(avi.display_balance).to eq("Your balance is $#{avi.balance}.")
+      expect(avi.display_balance).to eq("Your Balance is $#{avi.balance}.")
     end
   end
 
