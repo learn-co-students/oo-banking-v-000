0e26a10adbea937e37070af2a0878981e46fe6e9
ktochow1 <ktochow1@binghamton.edu>
2018-12-09 21:42:00 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class BankAccount
+  
+attr_accessor :balance, :status
+attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(amount)
+   @balance += amount 
+  end
+  
+  def display_balance
+    "Your balance is $#{balance}."
+  end  
+  
+  def valid?
+    if @status == "open" && @balance > 0
+      true 
+    else
+    false
+    end
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,52 @@
+require 'pry'
 class Transfer
-  # your code here
+attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?
+    sender.valid? && receiver.valid? && sender.balance >= self.amount
+    # does sender balance(100) have equal to or greater than amount to transfer(30)
+
+    
+  end
+  
+  def execute_transaction
+    if !self.valid?
+        self.status=("rejected")
+        "Transaction rejected. Please check your account balance."
+        
+    elsif self.status == "pending"
+      
+      receiver.balance += @amount
+      sender.balance -= @amount
+      self.status=("complete")
+    end
+  end
+
+  def reverse_transfer
+
+      
+       if self.status == "complete" 
+        sender.balance += @amount
+        receiver.balance -= @amount
+        self.status=("reversed")
+        end
+      
+      # binding.pry
+      #binding.pry sender and receiver balances are both 1000, why am i getting the wrong amount???
+      
+    
+    #make sure that both accounts are valid (y)
+    #make sure that reversal possible between executed transfers
+    #subtract money from receiver's account, add money to sender's account
+    
+    
+  end
+  
 end
