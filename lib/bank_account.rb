6fdfde55d4f40d1d5e7a8723185ce4d571dacab1
David Charles <ratlover03@gmail.com>
2019-01-30 14:47:17 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
+attr_accessor :balance, :status
+attr_reader  :name
+ def initialize(name, balance = 1000, status = "open")
+   @name = name
+   @balance = balance
+   @status = status
+ end
+ def deposit(amount)
+   @balance += amount
+ end
 
+ def display_balance
+   "Your balance is $#{@balance}."
+ end
+
+ def valid?
+   if @balance > 0 && @status == "open"
+     true
+   else
+     false
+   end
+ end
+
+    def close_account
+      @status = "closed"
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+def valid?
+  if @sender.valid? && @receiver.valid?
+    true
+  else
+    false
+  end
+end
+ def execute_transaction
+
+     if @status == "pending"
+  @sender.balance -= @amount
+  @receiver.balance += @amount
+  @status = "complete"
+end
+  if @sender.balance < @amount 
+      @status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+
+end
+
+
+ def reverse_transfer
+    if @status == "complete" && @receiver.balance >= @amount && @receiver.status == "open"
+      @sender.balance += @amount
+    @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
+
 end
