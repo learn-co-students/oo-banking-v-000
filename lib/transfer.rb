3a7e501ab69058b2d1f2cb692ef9cb8f234ca213
Sarah Paz <sarah.paz@gmail.com>
2019-02-02 05:45:19 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance >= @amount && @status == "pending"
+    if @sender.balance >= @amount 
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
