1639ecb2fa845fd0fe15ff1c6521f0d43b8f388f
Sarah Paz <sarah.paz@gmail.com>
2019-02-02 04:18:49 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,31 @@
 class Transfer
-  # your code here
+  attr_accessor :amount, :status
+  attr_reader :sender, :receiver, :bank_account
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    self.amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transation(amount)
+    # sender amount moved to receiver balance (complete)
+    if condition
+
+    # transfer can only happen once (complete)
+    elsif condition
+
+    # if sender account is not valid, reject transfer (rejected)
+    else
+
+    end
+
+
+  end
 end
