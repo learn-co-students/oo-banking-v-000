74269896d39e8dc215da916ef07fd215135e870a
Niusha H <niusha.hadziomerovic@gmail.com>
2019-02-23 19:01:04 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,8 +12,27 @@ attr_reader :sender, :name, :amount, :receiver
     @transactions = []
   end 
 
-  def valid?(receiver)
-    @sender == sender && @receiver == receiver
+  def valid?
+    @sender.valid? && @receiver.valid?
   end 
+
+  def execute_transaction
+    if @sender.balance >= @amount && @status != "complete"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    elsif @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end 
+
+  def reverse_transfer
+    if @sender.balance >= @amount && @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
 
 end
