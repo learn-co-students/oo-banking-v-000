bea3a6e9a443dd2fadcffca6274f58de67768ecc
Toni-Ty <antonique2479@yahoo.com>
2019-04-04 03:43:42 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,11 +15,15 @@ def valid?
 end
 
 def execute_transaction
-  @sender.balance -= @amount
-  @receiver.balance += @amount
-  if sender
-  @status = "complete"
+  if @sender.balance > @amount && @status == "pending"
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = "complete"
+else
+  @status = "rejected"
+  "Transaction rejected. Please check your account balance."
 end
+
 
 
 
