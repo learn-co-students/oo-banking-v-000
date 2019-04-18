18ec6730a711142372c7218a2ad0ed16079cbd95
Drake Rentrop <drakerentrop@gmail.com>
2019-04-18 19:17:38 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def balance
+    @balance
+  end
+
+def status
+  @status
+end
+
+def deposit(amount)
+  @balance += amount
+end
+
+def display_balance
+  "Your balance is $#{@balance}."
+end
+
+def valid?
+  if @balance > 0
+    true
+  else
+    false
+  end
+end
 
 end
