cac56f3cd22f765713624ac0b615aafa5c11fea8
Dan Suarez <suarez.daniel101@gmail.com>
2019-06-04 21:02:18 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,19 +1,37 @@
 class BankAccount
+  attr_accessor :balance
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name, balance = 1000)
     @name = name
-    @balance = 1000
+    @balance = balance
     @status = 'open'
     # binding.pry
   end
 
-  def balance
-    @balance
-  end
+  # def balance
+  #   @balance
+  # end
 
   def status
     @status
   end
 
+  def name=(name)
+    raise NoMethodError if @name != name
+    "NameError"
+  end
+
+  def deposit(money)
+    @balance = balance + money
+    # binding.pry
+  end
+
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  def valid?
+
+  end
 end
