814bed5f9cd0bd57443b1d6e49545666710df605
Dan Suarez <suarez.daniel101@gmail.com>
2019-06-04 19:03:42 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,19 @@
 class BankAccount
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+    # binding.pry
+  end
+
+  def balance
+    @balance
+  end
+
+  def status
+    @status
+  end
 
 end
