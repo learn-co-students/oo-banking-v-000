da1328f3135914ab1d14786192015ececfac309d
Dan Suarez <suarez.daniel101@gmail.com>
2019-06-06 19:41:04 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,15 @@
 class Transfer
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
   # your code here
+  def valid?
+
+  end
+
 end
