d142004357ffabb7400d9b93f9cb44ab69c4b16c
Jamal Gelin <gelin.jamal@gmail.com>
2019-07-02 03:23:27 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,12 @@
 class BankAccount
 
+  attr_accessor :balance, :status;
+  attr_reader :name;
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
   # your code here
+  attr_accessor :status, :amount;
+  attr_reader :sender, :receiver;
+
+  def initialize(sender, receiver, amount)
+  end
+
 end
