2f57f4297ed6f7699dd0ef800f61ab0f47908e05
Jamal Gelin <gelin.jamal@gmail.com>
2019-07-02 04:34:03 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,26 +11,25 @@ class Transfer
   end
 
   def valid?
-      @self.sender.valid? && @self.receiver.valid?
+    self.sender.valid? && self.receiver.valid?
   end
-
 
   def execute_transaction
-    if valid? && @self.sender.balance > @self.amount && @self.status != "complete"
-        @self.sender.balance -= @self.amount
-        @self.receiver.balance += @self.amount
-        @self.status = "complete"
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
     else
-        @self.status = 'rejected'
-        "Transaction rejected. Please check your account balance."
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
   end
-
 
   def reverse_transfer
     if execute_transaction
       self.sender.balance += self.amount
       self.receiver.balance -= self.amount
+      self.status = "reversed"
     end
   end
 
