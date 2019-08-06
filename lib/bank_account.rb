cc72c27a1bf78c54de28d2287056a7d807f72777
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-06 00:46:53 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,14 +1,38 @@
 class BankAccount
 
+attr_reader :name
+attr_accessor :balance, :status
 
-attr_accessor :name, :balance, :status
-
-  def intialize(name)
+  def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
 
+
   end
+
+  def deposit(money)
+   @balance = @balance + money
+  end
+
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0
+    return true
+    else
+    return false
+    end
+
+end
+
+    def close_account(name)
+      @name = name
+      @status = "closed"
+    end
+
 
 
 
