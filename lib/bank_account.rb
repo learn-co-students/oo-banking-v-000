fe776d752a6037fd444968980a8678ca7a37e430
Paris Dolfman <parisdolfman@gmail.com>
2019-08-14 19:54:39 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,16 @@
 class BankAccount
 
+attr_reader :name, :status, :balance
+
+def initialize(name)
+  @name = name
+  @balance = 1000
+  @status = "open"
+end
+
+def deposit(money)
+  @money = money
+  @balance += @money
+end
+
 end
