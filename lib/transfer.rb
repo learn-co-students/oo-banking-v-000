0fcc3cad77244a9eab2664c826c80ee2c95dbe15
Paris Dolfman <parisdolfman@gmail.com>
2019-08-15 19:02:41 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+
+attr_accessor :sender, :receiver, :amount
+
+def initialize(sender, receiver, amount)
+ @amount = amount
+ @sender = sender
+ @receiver = receiver
+end
+
+
 end
