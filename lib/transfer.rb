55e7bac0366eeb553017b724c2ca562c0954f58e
Jaime Johnsen <drivetraindiva@gmail.com>
2019-09-30 12:58:35 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,18 @@
 class Transfer
 
-def initialize(transfer)
+
+attr_accessor :sender, :receiver, :status, :amount
+
+def initialize(sender, receiver, transfer)
+  @sender = sender
+  @receiver = receiver
   @transfer = transfer
+  @status = "pending"
+  @amount = 50
 end
 
+def valid?(transfer)
+   @status == "pending" && @amount > 0
+
+end
 end
