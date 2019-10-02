f165e00b3097d84aae3368c6027c2b6400beff43
Jeremy Schuurmans <schuurm@ns.codes>
2019-10-02 16:16:04 -0700

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,12 +1,11 @@
 class BankAccount
-
   attr_accessor :balance, :status
   attr_reader :name
 
   def initialize(name)
-    @name = name
-    @balance = 1000
-    @status = "open"
+    @name = name;
+    @balance = 1000;
+    @status = "open";
   end
 
   def deposit(amount)
@@ -14,19 +13,14 @@ class BankAccount
   end
 
   def display_balance
-    return "Your balance is $#{@balance}."
+    "Your balance is $#{@balance}."
   end
 
   def valid?
-    if @status == "open" && @balance > 0
-      return true
-    else
-      return false
-    end
+    @status == "open" && @balance > 0 ? true : false
   end
 
   def close_account
     @status = "closed"
   end
-
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,34 +1,36 @@
-class Transfer
+require 'pry'
 
-  attr_accessor :sender, :receiver, :amount, :status
+class Transfer
+  attr_accessor :sender, :receiver, :status, :amount
 
   def initialize(sender, receiver, amount)
-    @sender = sender
-    @receiver = receiver
-    @amount = amount
-    @status = "pending"
+    @sender = sender;
+    @receiver = receiver;
+    @status = "pending";
+    @amount = amount;
   end
 
   def valid?
-    @sender.valid? && @receiver.valid? ? true : false
+    @sender.valid? && @receiver.valid? && @amount < @sender.balance ? true : false
   end
 
   def execute_transaction
-   if @sender.balance > @amount && @status == "pending"
-     @sender.balance -= @amount
-     @receiver.balance += @amount
-     @status = "complete"
-   else
-     @status = "rejected"
-     return "Transaction rejected. Please check your account balance."
-   end
- end
+    if valid? && @status != "complete"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else	   
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
 
   def reverse_transfer
     if @status == "complete"
-      @receiver.balance -= @amount
       @sender.balance += @amount
+      @receiver.balance -= @amount
       @status = "reversed"
     end
   end
+
 end
