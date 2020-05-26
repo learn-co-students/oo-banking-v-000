06ab301d27ecababc5e585b2e1d0e272361434b8
idith almog <almog.idith@gmail.com>
2020-05-26 19:59:34 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
 class BankAccount
-
-end
+ attr_reader :name
+attr_accessor :balance, :status
+ def initialize(name,balance=1000,status="open")
+   @name=name
+   @balance = balance
+   @status = status
+ end
+ def deposit(number)
+   @balance += number
+ end
+ def display_balance
+   return "Your balance is $#{@balance}."
+ end
+ def close_account
+   @status = "closed"
+ end
+ def valid?
+   if @status == "open" && @balance > 0
+     true
+   else
+     false
+   end
+ end
+ end
