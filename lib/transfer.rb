352d053688ab5d22adf884c32a72b0ab7c76a85c
Jaime Johnsen <drivetraindiva@gmail.com>
2020-07-14 17:58:12 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,10 +17,16 @@ def valid?
 end
 
 def execute_transaction
-  @sender.deposit(-@transfer)
-  @receiver.deposit(@transfer)
-  @status = "complete"
-
+  if @status == "pending"
+    if @sender.balance >= @amount
+      @sender.deposit(-@amount)
+      @receiver.deposit(@amount)
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
 end
 
 end
