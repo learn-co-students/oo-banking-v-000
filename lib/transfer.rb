089d9d849da9c2fb58b29c8e0d1308438e9d4b8b
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2020-11-04 09:07:59 -0500

    commit tree feff8cb86f402694aec5dca92dafde228f5e367e

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,16 +18,23 @@ class Transfer
         self.receiver.balance = self.amount + self.receiver.balance 
         self.sender.balance = self.sender.balance - self.amount 
         self.status = 'complete'
-   	  elsif self.sender != valid?
+   	  elsif self.sender.balance <= 0 || self.sender.status != 'complete'
    	  	self.status = 'rejected'
    	  	return "Transaction rejected. Please check your account balance."
+      elsif self.receiver.balance <= 0 || self.receiver.status != 'complete'
+        self.status = 'rejected'
+        return "Transaction rejected. Please check your account balance."
+      else
+        return "Not a valid transaction."
    	  end
    end
 
    def reverse_transfer
+   	if self.status == 'complete'
    	  self.receiver.balance = self.receiver.balance - self.amount 
    	  self.sender.balance = self.sender.balance + self.amount 
    	  self.status = 'reversed'
+   	end
    end
 
    def valid?
